e42574bde57291832e22dff44b6f926182c32bc0
Evgeniy Mineev <e.mineev@crazypanda.ru>
2025-06-03 01:01:50 +0400

    fix game path showing on phones

--- app/src/main/java/com/mobilerpgpack/phone/ui/items/PreferenceItem.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/items/PreferenceItem.kt
@@ -1,26 +1,40 @@
 package com.mobilerpgpack.phone.ui.items
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun PreferenceItem(title: String, initialValue: String = "", onClick : () -> Unit = {}) {
-    Row(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .clickable { onClick() }
             .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(title, Modifier.weight(1f))
-        Text(initialValue, style = MaterialTheme.typography.bodyMedium)
+        Text(
+            text = title,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+
+        if (initialValue.isNotBlank()) {
+            Spacer(modifier = Modifier.height(4.dp)) // немного отступа
+            Text(
+                text = initialValue,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
     }
 }
