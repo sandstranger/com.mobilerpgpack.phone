55697708c07e866a842cbe601ef8ca60ccbfe6e0
Evgeniy Mineev <e.mineev@crazypanda.ru>
2025-06-02 22:16:48 +0400

    common settings for wolfenstein rpg

--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/SettingsScreen.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/SettingsScreen.kt
@@ -1,45 +1,103 @@
 package com.mobilerpgpack.phone.ui.screen
 
 import android.content.Context
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mobilerpgpack.phone.R
 import com.mobilerpgpack.phone.engine.EngineTypes
-import com.mobilerpgpack.phone.ui.items.SwitchPreferenceItem
 import com.mobilerpgpack.phone.ui.items.ListPreferenceItem
+import com.mobilerpgpack.phone.ui.items.PreferenceItem
+import com.mobilerpgpack.phone.ui.items.SwitchPreferenceItem
 import com.mobilerpgpack.phone.utils.PreferencesStorage
+import com.mobilerpgpack.phone.utils.isTelevision
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @Composable
 fun SettingsScreen() {
+    val context = LocalContext.current;
+
+    if (context.isTelevision){
+        DrawTelevisionSettings(context)
+        return
+    }
+
+    DrawPhoneSettings(context)
+}
+
+@Composable
+private fun DrawTelevisionSettings (context: Context){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+        ) {
+            Text(context.getString(R.string.start_game), fontSize = 25.sp)
+        }
+
+        DrawAllSettings(context)
+    }
+}
+
+@Composable
+private fun DrawPhoneSettings (context: Context){
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {}
+            ) {
+                Icon(Icons.Default.PlayArrow, contentDescription = context.getString(R.string.start_game))
+            }
+        }
+    ) { innerPadding ->
+        DrawAllSettings(context, innerPadding)
+    }
+}
+
+@Composable
+private fun DrawAllSettings (context: Context) {
+    DrawAllSettings(context, PaddingValues())
+}
+
+@Composable
+private fun DrawAllSettings (context: Context, innerPadding: PaddingValues){
     val scope = rememberCoroutineScope()
     val scrollState = rememberScrollState()
-    val context = LocalContext.current;
 
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .padding(innerPadding)
             .padding(16.dp)
-            .verticalScroll(scrollState)
+            .verticalScroll(scrollState),
     ) {
         DrawCommonSettings(context, scope)
-
+        DrawWolfensteinRpgSettings(context, scope)
     }
 }
+
 @Composable
 private fun DrawCommonSettings (context: Context, scope: CoroutineScope){
 
@@ -90,6 +148,37 @@ private fun DrawCommonSettings (context: Context, scope: CoroutineScope){
     }
 
     HorizontalDivider()
+
+    SwitchPreferenceItem(context.getString(R.string.use_custom_file_picker),
+        checkedFlow = PreferencesStorage.getUseCustomFilePickerValue(context),
+    ) { newValue ->
+        scope.launch {
+            PreferencesStorage.setUseCustomFilePickerValue(context, newValue)
+        }
+    }
+
+    HorizontalDivider()
+}
+
+@Composable
+private fun DrawWolfensteinRpgSettings (context: Context,scope: CoroutineScope){
+    Text(context.getString(R.string.wolfenstein_rpg_settings), style = MaterialTheme.typography.titleLarge)
+
+    PreferenceItem(context.getString(R.string.wolfenstein_rpg_ipa_file))
+
+    HorizontalDivider()
+
+    PreferenceItem(context.getString(R.string.configure_screen_controls))
+
+    HorizontalDivider()
+
+    SwitchPreferenceItem(context.getString(R.string.hide_custom_screen_controls),
+        checkedFlow = PreferencesStorage.getHideWolfensteinRpgScreenControlsValue(context),
+    ) { newValue ->
+        scope.launch {
+            PreferencesStorage.setHideWolfensteinRpgScreenControlsValue(context, newValue)
+        }
+    }
 }
 
 @Composable
