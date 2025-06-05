61386a59da2c077d1d9875e2e04033b45ff75980
Evgeniy Mineev <e.mineev@crazypanda.ru>
2025-06-05 22:23:49 +0400

    allow to display in safe area in screen controls editor

--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/SettingsScreen.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/SettingsScreen.kt
@@ -2,7 +2,6 @@ package com.mobilerpgpack.phone.ui.screen
 
 import CustomTopBar
 import android.content.Context
-import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
@@ -28,8 +27,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.produceState
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -44,7 +41,7 @@ import com.mobilerpgpack.phone.engine.defaultPathToLogcatFile
 import com.mobilerpgpack.phone.engine.enginesInfo
 import com.mobilerpgpack.phone.engine.logcatFileName
 import com.mobilerpgpack.phone.engine.startEngine
-import com.mobilerpgpack.phone.ui.activity.ConfigureControlsActivity
+import com.mobilerpgpack.phone.ui.activity.ScreenControlsEditorActivity
 import com.mobilerpgpack.phone.ui.items.EditTextPreferenceItem
 import com.mobilerpgpack.phone.ui.items.ListPreferenceItem
 import com.mobilerpgpack.phone.ui.items.PreferenceItem
@@ -250,7 +247,7 @@ private fun DrawUserInterfaceSettings(context: Context, scope: CoroutineScope){
     HorizontalDivider()
 
     PreferenceItem(context.getString(R.string.configure_screen_controls)) {
-        ConfigureControlsActivity.startActivity(context, activeEngine)
+        ScreenControlsEditorActivity.startActivity(context, activeEngine)
     }
 
     HorizontalDivider()
