60a55fdc9d24e9e50a124000ad824b1d5b2932bd
Evgeniy Mineev <e.mineev@crazypanda.ru>
2025-06-05 22:23:49 +0400

    allow to display in safe area in screen controls editor

--- app/src/main/java/com/mobilerpgpack/phone/ui/activity/ScreenControlsEditorActivity.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/activity/ScreenControlsEditorActivity.kt
@@ -7,23 +7,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
-import androidx.core.view.WindowCompat
 import com.mobilerpgpack.phone.engine.EngineTypes
+import com.mobilerpgpack.phone.engine.activity.EngineActivity
 import com.mobilerpgpack.phone.engine.enginesInfo
 import com.mobilerpgpack.phone.engine.setFullscreen
 import com.mobilerpgpack.phone.ui.screen.OnScreenController
+import com.mobilerpgpack.phone.utils.PreferencesStorage
+import com.mobilerpgpack.phone.utils.displayInSafeArea
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import java.io.File
 
-class ConfigureControlsActivity : ComponentActivity() {
+class ScreenControlsEditorActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setFullscreen(window.decorView)
         super.onCreate(savedInstanceState)
 
+        var displayInSafeArea = false
+
+        runBlocking {
+            displayInSafeArea = PreferencesStorage.getDisplayInSafeAreaValue(this@ScreenControlsEditorActivity).first()!!
+        }
+
+        if (displayInSafeArea){
+            this.displayInSafeArea()
+        }
+
         setContent {
             MaterialTheme {
                 OnScreenController(enginesInfo[getSelectedEngineType()]!!.buttonsToDraw,
                     inGame = false){
-                    this@ConfigureControlsActivity.finish()
+                    this@ScreenControlsEditorActivity.finish()
                 }
             }
         }
@@ -42,7 +57,7 @@ class ConfigureControlsActivity : ComponentActivity() {
         private const val EXTRA_ENGINE_TYPE = "extra_engine_type"
 
         fun startActivity(context: Context, engineType: EngineTypes) {
-            with (Intent(context, ConfigureControlsActivity::class.java)){
+            with (Intent(context, ScreenControlsEditorActivity::class.java)){
                 this.putExtra(EXTRA_ENGINE_TYPE, engineType)
                 context.startActivity(this)
             }
