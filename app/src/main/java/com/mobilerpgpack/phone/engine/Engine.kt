7d07dfb559914dd2c19b38df4d3dc0a31486724e
Evgeniy Mineev <e.mineev@crazypanda.ru>
2025-06-08 16:52:31 +0400

    doom rpg screen buttons

--- app/src/main/java/com/mobilerpgpack/phone/engine/Engine.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/Engine.kt
@@ -7,6 +7,7 @@ import android.view.View
 import com.afollestad.materialdialogs.MaterialDialog
 import com.mobilerpgpack.phone.R
 import com.mobilerpgpack.phone.engine.activity.EngineActivity
+import com.mobilerpgpack.phone.ui.screen.doomRPGButtons
 import com.mobilerpgpack.phone.ui.screen.wolfensteinButtons
 import com.mobilerpgpack.phone.utils.PreferencesStorage
 import com.mobilerpgpack.phone.utils.startActivity
@@ -21,7 +22,7 @@ internal val enginesInfo : HashMap<EngineTypes, EngineInfo> = hashMapOf(
     EngineTypes.DoomRpg to EngineInfo("libDoomRPG.so", arrayOf("c++_shared","omp","FLAC","gio-2.0","glib-2.0","gmodule-2.0","gobject-2.0",
         "gthread-2.0","instpatch-1.0","oboe","ogg","opus","pcre","pcreposix","sndfile","vorbis","vorbisenc",
         "vorbisfile","fluidsynth-assetloader","fluidsynth","GL","SDL2","gme","SDL2_mixer","DoomRPG"),
-        wolfensteinButtons, pathToResourcesCallback = { context -> PreferencesStorage.getPathToDoomRpgZipFileValue (context) }),
+        doomRPGButtons, pathToResourcesCallback = { context -> PreferencesStorage.getPathToDoomRpgZipFileValue (context) }),
     EngineTypes.Doom2Rpg to EngineInfo("", arrayOf(""),
         wolfensteinButtons, pathToResourcesCallback = { context -> PreferencesStorage.getPathToWolfensteinRpgIpaFileValue (context) })
 )
