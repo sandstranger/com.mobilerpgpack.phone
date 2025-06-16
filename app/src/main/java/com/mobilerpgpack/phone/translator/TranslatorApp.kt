19793ed1374c597dd24553c6f696ccdce5b16aaf
Evgeniy Mineev <e.mineev@crazypanda.ru>
2025-06-17 00:45:58 +0400

    sql classes

--- app/src/main/java/com/mobilerpgpack/phone/translator/TranslatorApp.kt
+++ app/src/main/java/com/mobilerpgpack/phone/translator/TranslatorApp.kt
@@ -4,10 +4,10 @@ import android.app.Application
 import android.content.res.Configuration
 import android.os.Build
 
-class TranslatorApp : Application() {
+sealed class TranslatorApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        Translator.init(this)
+        TranslationManager.init(this)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
@@ -17,6 +17,11 @@ class TranslatorApp : Application() {
         else
             newConfig.locale.language
 
-        Translator.setLocale(newLang)
+        TranslationManager.setLocale(newLang)
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        TranslationManager.terminate()
     }
 }
