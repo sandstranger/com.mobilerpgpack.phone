ea01b5f30f09a53dac843d92b6bc355ce4cdaa0b
Evgeniy Mineev <e.mineev@crazypanda.ru>
2025-06-21 21:33:34 +0400

    simplify translationmanager

--- app/src/main/java/com/mobilerpgpack/phone/translator/sql/TranslationEntry.kt
+++ app/src/main/java/com/mobilerpgpack/phone/translator/sql/TranslationEntry.kt
@@ -1,18 +1,20 @@
-package com.mobilerpgpack.phone.translator
+package com.mobilerpgpack.phone.translator.sql
 
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.mobilerpgpack.phone.engine.EngineTypes
+import com.mobilerpgpack.phone.translator.models.TranslationType
 
 @Entity(
     tableName = "translations",
-    indices = [Index(value = ["key", "lang", "engine"], unique = true)]
+    indices = [Index(value = ["key", "lang", "engine","translationModelType"], unique = true)]
 )
 data class TranslationEntry(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val key: String,
     val lang: String,
     val value: String,
-    val engine: EngineTypes
+    val engine: EngineTypes,
+    val translationModelType : TranslationType
 )
