b28af1dc185334ce13b950f1728d51c0d9dd5636
Evgeniy Mineev <e.mineev@crazypanda.ru>
2025-06-22 15:55:43 +0400

    progress downloading support

---
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/DownloadViewModel.kt
@@ -0,0 +1,49 @@
+package com.mobilerpgpack.phone.ui.screen
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.mobilerpgpack.phone.translator.TranslationManager
+import com.mobilerpgpack.phone.translator.TranslatorApp
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class DownloadViewModel(
+) : ViewModel() {
+
+    var isLoading by mutableStateOf(false)
+
+    var downloadProgress by mutableStateOf("")   // будет хранить, например, "25%"
+        private set
+
+    private var downloadJob: Job? = null
+
+    fun startDownload() {
+        if (isLoading) return  // уже идёт
+
+        isLoading = true
+
+        if (downloadJob!=null){
+            return
+        }
+
+        downloadJob = TranslatorApp.globalScope.launch {
+            try {
+                downloadProgress = ""
+                TranslationManager.downloadModelIfNeeded { newValue ->
+                    downloadProgress = newValue
+                }
+            } finally {
+                isLoading = false
+            }
+        }
+    }
+
+    fun cancelDownload() {
+        isLoading = false
+        downloadJob?.cancel()
+        downloadJob = null
+        TranslationManager.cancelDownloadModel()
+    }
+}
