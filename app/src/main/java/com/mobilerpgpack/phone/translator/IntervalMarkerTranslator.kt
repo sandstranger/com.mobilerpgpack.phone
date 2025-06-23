c190470bd7f975408b7822d09aa20aeb21cb4d20
Evgeniy Mineev <e.mineev@crazypanda.ru>
2025-06-24 00:10:47 +0400

    special symboles translator

---
+++ app/src/main/java/com/mobilerpgpack/phone/translator/IntervalMarkerTranslator.kt
@@ -0,0 +1,53 @@
+package com.mobilerpgpack.phone.translator
+
+import android.util.Log
+
+class IntervalMarkerTranslator {
+
+    private val specialMarkers = hashSetOf("\n", "|")
+
+    suspend fun translateWithFixedInterval(
+        text: String,
+        translateFn: suspend (String) -> String
+    ): String {
+
+        var textCopy = text
+
+        val markersIntervals = specialMarkers
+            .mapNotNull { marker ->
+                val idx = text.indexOf(marker)
+                if (idx >= 0) marker to idx else null
+            }
+            .toMap()
+
+        if (!markersIntervals.isEmpty()) {
+            specialMarkers.forEach {
+                textCopy = textCopy.replace(it, " ")
+            }
+        }
+
+        Log.d("TEXT_TO_TRANSLATE", textCopy)
+
+        var translatedText = translateFn(textCopy)
+
+        Log.d("TEXT_TO_TRANSLATE", translatedText)
+
+        if (textCopy == translatedText){
+            return text
+        }
+
+        val resultBuilder = StringBuilder(translatedText.length * 2 )
+
+        translatedText.forEachIndexed { index, c ->
+            resultBuilder.append(c)
+
+            for ((marker, interval) in markersIntervals) {
+                if ((index + 1) % interval == 0) {
+                    resultBuilder.append(marker)
+                }
+            }
+        }
+
+        return resultBuilder.toString()
+    }
+}
