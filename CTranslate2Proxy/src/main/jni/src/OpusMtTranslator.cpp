094b8290cde3f9367f76e69aafaee56b9814602b
Evgeniy Mineev <e.mineev@crazypanda.ru>
2025-06-27 20:55:10 +0400

    multithread support for m2m and small100

--- CTranslate2Proxy/src/main/jni/src/OpusMtTranslator.cpp
+++ CTranslate2Proxy/src/main/jni/src/OpusMtTranslator.cpp
@@ -12,7 +12,7 @@ using namespace sentencepiece;
 using namespace ctranslate2;
 
 extern TranslationOptions create_translation_options();
-extern unique_ptr<Translator> create_translator (string model_path, bool multi_thread = false );
+extern unique_ptr<Translator> create_translator (string model_path, bool multi_thread = true );
 extern vector<vector<string>> tokenize_sentences(SentencePieceProcessor *tokenizer,
                                                  const vector<string> *sentences);
 extern string decode(SentencePieceProcessor *tokenizer, vector<TranslationResult> results);
