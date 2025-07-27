0f0cb12ad99918779b41b691ddeeda880d7748ae
sandstranger <sandstranger@gmail.com>
2025-07-27 17:38:35 +0400

    save regexes to consts variables

--- CTranslate2Proxy/src/main/java/com/mobilerpgpack/ctranslate2proxy/Translator.kt
+++ CTranslate2Proxy/src/main/java/com/mobilerpgpack/ctranslate2proxy/Translator.kt
@@ -14,15 +14,15 @@ abstract class Translator {
     abstract fun release()
 
     protected fun splitTextIntoSentences(text: String): List<String> {
+        val replacedText = text.replace(dotsWithSpacingRegex,"$0 ")
         val iterator = BreakIterator.getSentenceInstance(Locale.US)
         iterator.setText(text)
-
         val sentences = mutableListOf<String>()
         var start = iterator.first()
         var end = iterator.next()
 
         while (end != BreakIterator.DONE) {
-            val sentence = text.substring(start, end).trim()
+            val sentence = replacedText.substring(start, end).trim()
             if (sentence.isNotEmpty()) {
                 sentences.addAll(splitSentencesRegex(sentence))
             }
@@ -37,11 +37,13 @@ abstract class Translator {
     }
 
     private fun splitSentencesRegex(text: String): List<String> {
-        val regex = Regex("(?<=[.!?])[\"')\\]]*\\s+(?=[A-Z0-9\\-])")
-        return text.split(regex).map { it.trim() }.filter { it.isNotEmpty() }
+        return text.split(dotsRegex).map { it.trim() }.filter { it.isNotEmpty() }
     }
 
     private companion object{
+        private val dotsRegex = Regex("(?<=[.!?])[\"')\\]]*\\s+(?=[A-Z0-9\\-])")
+        private val dotsWithSpacingRegex = Regex("[.!?](?=\\p{L})")
+
         init {
             System.loadLibrary("omp")
             System.loadLibrary(if (BuildConfig.DEBUG) "spdlogd" else "spdlog")
