9044d7a6b788b5e5542ce20c19071442f3f64da1
sandstranger <sandstranger@gmail.com>
2025-09-07 02:20:02 +0400

    fixed changing navigationbar color

---
+++ app/src/main/java/com/mobilerpgpack/phone/ui/items/CustomNavigationBar.kt
@@ -0,0 +1,26 @@
+package com.mobilerpgpack.phone.ui.items
+
+import androidx.activity.compose.LocalActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.WindowCompat
+
+@Composable
+fun SetupNavigationBar(useDarkTheme: Boolean) {
+    val activity = LocalActivity.current!!
+
+    SideEffect {
+        val window = activity.window
+        val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+
+        window.navigationBarColor = if (useDarkTheme) {
+            Color(0xFF121212).toArgb()
+        } else {
+            Color.White.toArgb()
+        }
+        insetsController.isAppearanceLightNavigationBars = !useDarkTheme
+    }
+}
