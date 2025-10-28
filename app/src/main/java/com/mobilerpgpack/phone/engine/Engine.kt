46033b84e8ccb600b7964f5159be02de9104166f
sandstranger <sandstranger@gmail.com>
2025-10-28 15:32:41 +0400

    disable omw shader conv, because ng gl4es gles 3.2 shader conv is working now

--- app/src/main/java/com/mobilerpgpack/phone/engine/Engine.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/Engine.kt
@@ -26,9 +26,10 @@ val gl4esLibraryName : String
         return if (BuildConfig.LEGACY_GLES2) return "gl4es" else return "ng_gl4es"
     }
 
+
 val gl4esFullLibraryName get() = "lib${gl4esLibraryName}.so"
 
-internal val enginesInfo : HashMap<EngineTypes, EngineInfo> = hashMapOf(
+internal val enginesInfo : HashMap<EngineTypes, EngineInfo> = if (BuildConfig.LEGACY_GLES2) hashMapOf(
     EngineTypes.Doom64ExPlus to EngineInfo("libDOOM64.so","DOOM64",
         arrayOf(gl4esLibraryName,"SDL3",if (BuildConfig.DEBUG) "png16d" else "png16",
             "fmod","DOOM64"), wolfensteinButtons,
@@ -36,13 +37,29 @@ internal val enginesInfo : HashMap<EngineTypes, EngineInfo> = hashMapOf(
     EngineTypes.WolfensteinRpg to EngineInfo("libWolfensteinRPG.so","WolfensteinRPG",
         arrayOf(gl4esLibraryName,"SDL2","openal","SDL2_ttf","c++_shared","fbjni","Translator","WolfensteinRPG"), wolfensteinButtons, pathToResourcesCallback =
             { context -> PreferencesStorage.getPathToWolfensteinRpgIpaFileValue (context) } ),
-    EngineTypes.DoomRpg to EngineInfo("libDoomRPG.so","DoomRPG", arrayOf("fluidsynth","ng_gl4es","SDL2","gme","SDL2_mixer",
+    EngineTypes.DoomRpg to EngineInfo("libDoomRPG.so","DoomRPG", arrayOf(gl4esLibraryName,"fluidsynth","SDL2","gme","SDL2_mixer",
         "SDL2_ttf","c++_shared","fbjni","Translator","DoomRPG"), doomRPGButtons,
         pathToResourcesCallback = { context -> PreferencesStorage.getPathToDoomRpgZipFileValue (context) }),
     EngineTypes.Doom2Rpg to EngineInfo("libDoomIIRPG.so","DoomIIRPG",
         arrayOf(gl4esLibraryName,"SDL2","openal","SDL2_ttf","c++_shared","fbjni","Translator","DoomIIRPG"),
         doom2RPGButtons, pathToResourcesCallback = { context -> PreferencesStorage.getPathToDoom2RpgIpaFile (context) })
 )
+else
+    hashMapOf(
+        EngineTypes.Doom64ExPlus to EngineInfo("libDOOM64.so","DOOM64",
+            arrayOf("spirv-cross-c-shared",gl4esLibraryName,"SDL3",if (BuildConfig.DEBUG) "png16d" else "png16",
+                "fmod","DOOM64"), wolfensteinButtons,
+            pathToResourcesCallback = { context -> PreferencesStorage.getPathToDoom64MainWadsFolder (context) }),
+        EngineTypes.WolfensteinRpg to EngineInfo("libWolfensteinRPG.so","WolfensteinRPG",
+            arrayOf("spirv-cross-c-shared",gl4esLibraryName,"SDL2","openal","SDL2_ttf","c++_shared","fbjni","Translator","WolfensteinRPG"), wolfensteinButtons, pathToResourcesCallback =
+                { context -> PreferencesStorage.getPathToWolfensteinRpgIpaFileValue (context) } ),
+        EngineTypes.DoomRpg to EngineInfo("libDoomRPG.so","DoomRPG", arrayOf("spirv-cross-c-shared",gl4esLibraryName,"fluidsynth","SDL2","gme","SDL2_mixer",
+            "SDL2_ttf","c++_shared","fbjni","Translator","DoomRPG"), doomRPGButtons,
+            pathToResourcesCallback = { context -> PreferencesStorage.getPathToDoomRpgZipFileValue (context) }),
+        EngineTypes.Doom2Rpg to EngineInfo("libDoomIIRPG.so","DoomIIRPG",
+            arrayOf("spirv-cross-c-shared",gl4esLibraryName,"SDL2","openal","SDL2_ttf","c++_shared","fbjni","Translator","DoomIIRPG"),
+            doom2RPGButtons, pathToResourcesCallback = { context -> PreferencesStorage.getPathToDoom2RpgIpaFile (context) })
+    )
 
 internal val defaultPathToLogcatFile: String = "${Environment.getExternalStorageDirectory().absolutePath}" +
         "${File.separator}$logcatFileName"
@@ -70,7 +87,7 @@ suspend fun startEngine(context: Context, engineToPlay : EngineTypes) {
 }
 
 internal fun initializeCommonEngineData (context: Context){
-    Os.setenv("LIBGL_SIMPLE_SHADERCONV", "1", true)
+    Os.setenv("LIBGL_SIMPLE_SHADERCONV", "0", true)
     Os.setenv("LIBGL_DXTMIPMAP", "1", true)
     Os.setenv("LIBGL_ES",if (BuildConfig.LEGACY_GLES2) "2" else "3",true)
     Os.setenv("LIBGL_GL","21", true)
