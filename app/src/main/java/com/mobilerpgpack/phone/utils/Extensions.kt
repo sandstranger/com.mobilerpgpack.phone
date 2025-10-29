796c8ae3faf8e00f374926647e87cf5f1d8a8c54
sandstranger <sandstranger@gmail.com>
2025-10-29 20:23:25 +0400

    cast to bool in jna call

--- app/src/main/java/com/mobilerpgpack/phone/utils/Extensions.kt
+++ app/src/main/java/com/mobilerpgpack/phone/utils/Extensions.kt
@@ -22,10 +22,6 @@ import androidx.core.view.updatePadding
 val Context.isTelevision get() = this.packageManager.hasSystemFeature(PackageManager.FEATURE_LEANBACK)
 
 @Suppress("UNCHECKED_CAST")
-fun <T> com.sun.jna.Function.callAs(returnType: Class<T>, vararg args: Any?): T  =
-    this.invoke(returnType, args) as T
-
-@Suppress("UNCHECKED_CAST")
 fun <T> com.sun.jna.Function.callAs(returnType: Class<T>): T  = this.invoke(returnType, null) as T
 
 inline fun <reified T> Context.startActivity(finishParentActivity : Boolean = true) where T : Activity {
