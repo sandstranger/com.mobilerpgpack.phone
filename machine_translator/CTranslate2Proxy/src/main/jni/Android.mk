75f22eb8c671827b66bda461a6c7919dd9b6feae
sandstranger <sandstranger@gmail.com>
2025-10-30 21:09:16 +0400

    move all deps to prefabs

--- machine_translator/CTranslate2Proxy/src/main/jni/Android.mk
+++ machine_translator/CTranslate2Proxy/src/main/jni/Android.mk
@@ -4,10 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := CTranslate2Proxy
 
-LOCAL_C_INCLUDES :=                                     \
-	$(LOCAL_PATH)/../../../../../third_party/sentencepiece/src/main/jni/sentencepiece/src			\
-	$(LOCAL_PATH)/../../../../../third_party/CTranslate2/src/main/jni/CTranslate2/include		\
-
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
 LOCAL_SRC_FILES := \
@@ -17,19 +13,15 @@ LOCAL_SRC_FILES := \
 	$(LOCAL_PATH)/src/NLLB200Translator.cpp \
 	$(LOCAL_PATH)/src/Utils.cpp
 
-ifeq ($(APP_OPTIM),debug)
-	LOCAL_LDLIBS += $(LOCAL_PATH)/../../../../../third_party/CTranslate2/build/intermediates/merged_native_libs/debug/mergeDebugNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libctranslate2.so
-else
-	LOCAL_LDLIBS += $(LOCAL_PATH)/../../../../../third_party/CTranslate2/build/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libctranslate2.so
-endif
-
-ifeq ($(APP_OPTIM),debug)
-	LOCAL_LDLIBS += $(LOCAL_PATH)/../../../../../third_party/sentencepiece/build/intermediates/merged_native_libs/debug/mergeDebugNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libsentencepiece.so
-else
-	LOCAL_LDLIBS += $(LOCAL_PATH)/../../../../../third_party/sentencepiece/build/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libsentencepiece.so
-endif
-
 LOCAL_CPPFLAGS += -O3 -flto=thin -std=c++20 -fexceptions -frtti
 LOCAL_LDFLAGS += -flto=thin -Wl,-plugin-opt=-emulated-tls -fuse-ld=lld
 LOCAL_LDLIBS += -llog
+LOCAL_SHARED_LIBRARIES += ctranslate2
+LOCAL_SHARED_LIBRARIES += sentencepiece
 include $(BUILD_SHARED_LIBRARY)
+
+ifneq ($(call ndk-major-at-least,21),true)
+    $(call import-add-path,$(NDK_GRADLE_INJECTED_IMPORT_PATH))
+endif
+$(call import-module,prefab/CTranslate2)
+$(call import-module,prefab/sentencepiece)
