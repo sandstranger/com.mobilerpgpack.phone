2c9f10ec3a748e5bfda24fb6eb16ded5655f3af6
sandstranger <sandstranger@gmail.com>
2025-11-06 01:49:25 +0400

    new edit text prefs

--- app/src/main/java/com/mobilerpgpack/phone/ui/items/EditTextPreferenceItem.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/items/EditTextPreferenceItem.kt
@@ -1,23 +1,14 @@
 package com.mobilerpgpack.phone.ui.items
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.github.sproctor.composepreferences.TextPreference
 import com.mobilerpgpack.phone.R
 
 @Composable
@@ -25,54 +16,21 @@ fun EditTextPreferenceItem(
     title: String,
     value: String,
     hint: String = "",
-    onValueChange: (String) -> Unit
-) {
+    onValueChange: (String) -> Unit) {
     val context = LocalContext.current
-    var showDialog by rememberSaveable { mutableStateOf(false) }
+    val cancelString = context.getString(R.string.cancel_text)
+    val positiveString = context.getString(R.string.ok_text)
 
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .clickable { showDialog = true }
-        .padding(16.dp)) {
-
-        TranslatedText(text = title)
-
-        Text(
-            text = if (value.isNotEmpty()) value else hint,
+    TextPreference(
+        title = { Text(title) },
+        value = value,
+        onValueChanged = onValueChange,
+        summary = {  Text(
+            text = value.ifEmpty { hint },
             style = MaterialTheme.typography.bodyMedium,
             color = if (value.isNotEmpty()) Color.Unspecified else Color.Gray,
             modifier = Modifier.padding(top = 4.dp)
-        )
-    }
-
-    if (showDialog) {
-        var tempValue by rememberSaveable { mutableStateOf(value) }
-
-        AlertDialog(
-            onDismissRequest = { showDialog = false },
-            confirmButton = {
-                TextButton(onClick = {
-                    onValueChange(tempValue)
-                    showDialog = false
-                }) {
-                    TranslatedText(context.getString(R.string.ok_text))
-                }
-            },
-            dismissButton = {
-                TextButton(onClick = { showDialog = false }) {
-                    TranslatedText(context.getString(R.string.cancel_text))
-                }
-            },
-            title = { Text(text = title) },
-            text = {
-                OutlinedTextField(
-                    value = tempValue,
-                    onValueChange = { tempValue = it },
-                    placeholder = { Text(hint) },
-                    singleLine = true,
-                    modifier = Modifier.fillMaxWidth()
-                )
-            }
-        )
-    }
+        ) },
+        confirmText = positiveString,
+        dismissText = cancelString )
 }
