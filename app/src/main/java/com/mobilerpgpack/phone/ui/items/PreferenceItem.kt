b9a20fab919f629bb01a26ed4ef221c667c22c40
Evgeniy Mineev <e.mineev@crazypandagames.com>
2025-11-06 00:47:08 +0400

    custom prefs item

--- app/src/main/java/com/mobilerpgpack/phone/ui/items/PreferenceItem.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/items/PreferenceItem.kt
@@ -1,39 +1,22 @@
 package com.mobilerpgpack.phone.ui.items
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
+import com.github.sproctor.composepreferences.Preference
 
 @Composable
-fun PreferenceItem(title: String, initialValue: String = "", onClick : () -> Unit = {}) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { onClick() }
-            .padding(16.dp),
-    ) {
-        TranslatedText(
-            text = title,
-            overflow = TextOverflow.Ellipsis
-        )
-
-        if (initialValue.isNotBlank()) {
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = initialValue,
-                style = MaterialTheme.typography.bodySmall,
-                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                overflow = TextOverflow.Ellipsis
-            )
-        }
+fun PreferenceItem(title: String, initialValue: String = "", onClick: () -> Unit = {}) {
+    if (!initialValue.isEmpty()) {
+        Preference(
+            title = { Text(title, overflow = TextOverflow.Ellipsis) },
+            summary = { Text(initialValue) },
+            modifier = Modifier.clickable { onClick() })
+    } else {
+        Preference(
+            title = { Text(title, overflow = TextOverflow.Ellipsis) },
+            modifier = Modifier.clickable { onClick() })
     }
 }
