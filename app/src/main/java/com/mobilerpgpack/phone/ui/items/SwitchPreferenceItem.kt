7b13341b5bc32cc03684e0bddd5967ebe860dbfc
sandstranger <sandstranger@gmail.com>
2025-11-06 02:37:38 +0400

    simplify switch pref item

--- app/src/main/java/com/mobilerpgpack/phone/ui/items/SwitchPreferenceItem.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/items/SwitchPreferenceItem.kt
@@ -1,55 +1,33 @@
 package com.mobilerpgpack.phone.ui.items
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import com.github.sproctor.composepreferences.SwitchPreference
 import kotlinx.coroutines.flow.Flow
 
 @Composable
 fun SwitchPreferenceItem(
     title: String,
-    checkedFlow: Flow<Boolean?>,
-    enabled: Boolean = true,
-    onCheckedChange : (Boolean) -> Unit
-) {
-    val checked by checkedFlow.collectAsState(initial = false)
-    var checkedState by rememberSaveable (checked!!) { mutableStateOf(checked!!) }
+    initialValue: Boolean,
+    key: String,
+    enabled: Boolean = true) {
+    val titleColor = if (enabled) LocalContentColor.current else LocalContentColor.current.copy(alpha = 0.38f)
+    SwitchPreference(
+        title = { Text(title, color = titleColor) },
+        initialValue = initialValue,
+        key = key,
+        enabled = enabled)
+}
 
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable {
-                if (enabled) {
-                    checkedState = !checkedState
-                    onCheckedChange(checkedState)
-                }
-            }
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        TranslatedText(title,
-            modifier = Modifier.weight(1f),
-            color = if (enabled) LocalContentColor.current else  LocalContentColor.current.copy(alpha = 0.38f))
-        Spacer(Modifier.width(5.dp))
-        Switch(
-            checked = checkedState,
-            onCheckedChange = null,
-            enabled = enabled
-        )
-    }
+@Composable
+fun SwitchPreferenceItem(
+    title: String,
+    initialValueFlow: Flow<Boolean>,
+    key: String,
+    enabled: Boolean = true) {
+    val initialValue by initialValueFlow.collectAsState(initial = false)
+    SwitchPreferenceItem(title, initialValue, key, enabled)
 }
