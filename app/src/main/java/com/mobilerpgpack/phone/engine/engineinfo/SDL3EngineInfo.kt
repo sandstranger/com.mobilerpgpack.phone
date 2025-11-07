65652162addfc06edd44f66a02b23a47c25adf5b
sandstranger <sandstranger@gmail.com>
2025-11-07 23:29:31 +0400

    port SDL2 changes to SDL3, touch camera with SDL3 is not working yet

--- app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/SDL3EngineInfo.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/SDL3EngineInfo.kt
@@ -1,7 +1,12 @@
 package com.mobilerpgpack.phone.engine.engineinfo
 
 import com.mobilerpgpack.phone.engine.activity.SDL3GameActivity
+import com.mobilerpgpack.phone.ui.screen.screencontrols.IScreenController
 import com.mobilerpgpack.phone.ui.screen.screencontrols.IScreenControlsView
+import com.mobilerpgpack.phone.ui.screen.screencontrols.SDL3ScreenController
+import org.koin.core.component.inject
+import org.koin.core.qualifier.named
+import org.libsdl3.app.SDLSurface
 
 abstract class SDL3EngineInfo (private val mainEngineLib: String,
                       private val allLibs : Array<String>,
@@ -10,5 +15,11 @@ abstract class SDL3EngineInfo (private val mainEngineLib: String,
 
     override val gameActivityClazz: Class<*> = SDL3GameActivity::class.java
 
-    override fun loadControlsLayout() {}
+    override val screenController: IScreenController by inject (
+        named(SDL3ScreenController.SDL3_SCREEN_CONTROLLER_NAME))
+
+    override fun setScreenResolution(screenWidth: Int, screenHeight: Int) {
+        SDLSurface.fixedWidth = screenWidth
+        SDLSurface.fixedHeight = screenHeight
+    }
 }
--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/ScreenControlsViews.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/ScreenControlsViews.kt
@@ -110,6 +110,112 @@ val wolfensteinButtons : Collection<IScreenControlsView> = listOf(
     )
 )
 
+val doom64Buttons : Collection<IScreenControlsView> = listOf(
+    SDL3Dpad(
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.05f,
+        offsetYPercent = 0.5f,
+        sizePercent = 0.25f,
+    ),
+    SDL3ImageButton(
+        "attack",
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.8f,
+        offsetYPercent = 0.45f,
+        sizePercent = 0.13f,
+        buttonResId = R.drawable.attack_button,
+        sdlKeyEvent = KeyEvent.KEYCODE_ENTER
+    ),
+    SDL3ImageButton(
+        "next_weapon",
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.85f,
+        offsetYPercent = 0.1f,
+        sizePercent = 0.075f,
+        buttonResId = R.drawable.next_weapon,
+        sdlKeyEvent = KeyEvent.KEYCODE_Z
+    ),
+    SDL3ImageButton(
+        "prev_weapon",
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.85f,
+        offsetYPercent = 0.26f,
+        sizePercent = 0.075f,
+        buttonResId = R.drawable.prev_weapon,
+        sdlKeyEvent = KeyEvent.KEYCODE_X
+    ),
+    SDL3ImageButton(
+        "pass_turn",
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.97f,
+        offsetYPercent = 0.7f,
+        sizePercent = 0.085f,
+        buttonResId = R.drawable.pass_turn,
+        sdlKeyEvent = KeyEvent.KEYCODE_C
+    ),
+    SDL3ImageButton(
+        "automap",
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.83f,
+        offsetYPercent = 0.9f,
+        sizePercent = 0.085f,
+        buttonResId = R.drawable.automap,
+        sdlKeyEvent = KeyEvent.KEYCODE_TAB
+    ),
+    SDL3ImageButton(
+        "journal",
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.76f,
+        offsetYPercent = 0.26f,
+        sizePercent = 0.085f,
+        buttonResId = R.drawable.journal,
+        sdlKeyEvent = KeyEvent.KEYCODE_P
+    ),
+    SDL3ImageButton(
+        "items",
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.72f,
+        offsetYPercent = 0.9f,
+        sizePercent = 0.085f,
+        buttonResId = R.drawable.items,
+        sdlKeyEvent = KeyEvent.KEYCODE_I
+    ),
+    SDL3ImageButton(
+        "syringes",
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.7f,
+        offsetYPercent = 0.45f,
+        sizePercent = 0.085f,
+        buttonResId = R.drawable.syringe,
+        sdlKeyEvent = KeyEvent.KEYCODE_O
+    ),
+    SDL3ImageButton(
+        "escape",
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.99f,
+        offsetYPercent = 0.05f,
+        sizePercent = 0.07f,
+        buttonResId = R.drawable.pause,
+        sdlKeyEvent = KeyEvent.KEYCODE_ESCAPE
+    ),
+    ToggleImageButton(
+        ToggleImageButton.SHOW_KEYBOARD_BUTTON_ID,
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.75f,
+        offsetYPercent = 0.05f,
+        sizePercent = 0.08f,
+        buttonResId = R.drawable.keyboard
+    ),
+    ToggleImageButton(
+        ToggleImageButton.HIDE_CONTROLS_BUTTON_ID,
+        EngineTypes.Doom64ExPlus,
+        offsetXPercent = 0.5f,
+        offsetYPercent = 0.05f,
+        sizePercent = 0.06f,
+        buttonResId = R.drawable.toggles
+    )
+)
+
 val doomRPGButtons : Collection<IScreenControlsView> = listOf(
     SDL2Dpad(
         EngineTypes.DoomRpg,
