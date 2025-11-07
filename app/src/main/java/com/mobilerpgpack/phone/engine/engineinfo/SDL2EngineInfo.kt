acdf33453d6d273638e5971f2fbf32bdab6f6867
sandstranger <sandstranger@gmail.com>
2025-11-08 01:25:57 +0400

    port sdl2 cursor icon to SDL3

--- app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/SDL2EngineInfo.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/SDL2EngineInfo.kt
@@ -2,12 +2,14 @@ package com.mobilerpgpack.phone.engine.engineinfo
 
 import androidx.compose.runtime.Composable
 import com.mobilerpgpack.phone.engine.activity.SDL2GameActivity
-import com.mobilerpgpack.phone.ui.items.MouseIcon
+import com.mobilerpgpack.phone.ui.screen.screencontrols.MouseIcon
 import com.mobilerpgpack.phone.ui.screen.screencontrols.IScreenController
 import com.mobilerpgpack.phone.ui.screen.screencontrols.IScreenControlsView
+import com.mobilerpgpack.phone.ui.screen.screencontrols.SDL2MouseIcon
 import com.mobilerpgpack.phone.ui.screen.screencontrols.SDL2ScreenController
 import org.koin.core.component.inject
 import org.koin.core.qualifier.named
+import org.libsdl.app.SDLActivity
 import org.libsdl.app.SDLSurface
 
 abstract class SDL2EngineInfo(private val mainEngineLib: String,
@@ -15,15 +17,19 @@ abstract class SDL2EngineInfo(private val mainEngineLib: String,
                      private val buttonsToDraw : Collection<IScreenControlsView> ) :
     EngineInfo(mainEngineLib, allLibs, buttonsToDraw ) {
 
+    private val mouseIcon : SDL2MouseIcon by inject ()
+
     override val gameActivityClazz: Class<*> = SDL2GameActivity::class.java
 
     override val screenController: IScreenController by inject (
         named(SDL2ScreenController.SDL2_SCREEN_CONTROLLER_NAME))
 
+    override fun isMouseShown(): Int = SDLActivity.isMouseShown()
+
     @Composable
     override fun DrawMouseIcon() {
         super.DrawMouseIcon()
-        MouseIcon()
+        mouseIcon.DrawMouseIcon()
     }
 
     override fun setScreenResolution(screenWidth: Int, screenHeight: Int) {
