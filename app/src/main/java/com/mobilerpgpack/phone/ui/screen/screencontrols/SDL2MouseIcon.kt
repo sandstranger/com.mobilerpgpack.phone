e92505ee4ff4aa61e35c09798a0ddab9c898045d
sandstranger <sandstranger@gmail.com>
2025-11-08 01:40:02 +0400

    always get new values of width and height

--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/SDL2MouseIcon.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/SDL2MouseIcon.kt
@@ -5,9 +5,9 @@ import org.libsdl.app.SDLSurface
 
 class SDL2MouseIcon : MouseIcon() {
 
-    override val fixedWidth: Int = SDLSurface.fixedWidth
+    override val fixedWidth: Int get() = SDLSurface.fixedWidth
 
-    override val fixedHeight: Int = SDLSurface.fixedHeight
+    override val fixedHeight: Int get() = SDLSurface.fixedHeight
 
     override fun getMouseX(): Float = SDLActivity.getMouseX().toFloat()
 
