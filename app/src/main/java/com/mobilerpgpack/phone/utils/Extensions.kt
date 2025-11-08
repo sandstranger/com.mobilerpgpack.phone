eb4de06ccea4e7281767f3b1a6552f0cee05f737
sandstranger <sandstranger@gmail.com>
2025-11-09 00:17:16 +0400

    black screen navigation bar, when safe area is active fixed

--- app/src/main/java/com/mobilerpgpack/phone/utils/Extensions.kt
+++ app/src/main/java/com/mobilerpgpack/phone/utils/Extensions.kt
@@ -1,25 +1,20 @@
 package com.mobilerpgpack.phone.utils
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Application
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
-import android.graphics.Point
-import android.graphics.Rect
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
 import android.os.Environment
-import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
 import android.view.WindowInsets
 import android.view.WindowInsetsController
-import android.view.WindowManager
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -50,22 +45,47 @@ fun Context.startActivity(activityClazz : Class<*>, finishParentActivity : Boole
     }
 }
 
-fun Activity.getScreenResolution(): ScreenResolution {
+fun Activity.getScreenResolution(drawInSafeArea : Boolean = false): ScreenResolution {
     val windowMetrics = WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(this)
+    val insets = ViewCompat.getRootWindowInsets(window.decorView)?.getInsets(
+        WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
+        ?: androidx.core.graphics.Insets.NONE
+
     val bounds = windowMetrics.bounds
-    return ScreenResolution(bounds.width(), bounds.height())
+
+    if (!drawInSafeArea){
+        return ScreenResolution(bounds.width(), bounds.height())
+    }
+
+    return ScreenResolution(bounds.width() - insets.left - insets.right,
+        bounds.height() - insets.top - insets.bottom)
+}
+
+fun Activity.hideSystemBarsAndWait(callback: () -> Unit) {
+    val decorView = window.decorView
+
+    decorView.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+        override fun onGlobalLayout() {
+            decorView.postDelayed({
+                val rootInsets = ViewCompat.getRootWindowInsets(decorView)
+                if (rootInsets != null && !rootInsets.isVisible(WindowInsetsCompat.Type.systemBars())) {
+                    decorView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    callback()
+                }
+            }, 50)
+        }
+    })
+
+    hideSystemBars()
 }
 
 fun Activity.hideSystemBars() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        this.window.decorView.post {
             this.window.insetsController?.let {
                 it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
                 it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
-        }
     } else {
-        this.window.decorView.post {
             @Suppress("DEPRECATION")
             this.window.decorView.systemUiVisibility = (
                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -75,7 +95,6 @@ fun Activity.hideSystemBars() {
                             or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
         }
-    }
 }
 
 fun Context.isInternetAvailable(): Boolean {
@@ -112,7 +131,7 @@ fun Context.isExternalStoragePermissionGranted () : Boolean {
     ) == PackageManager.PERMISSION_GRANTED
 }
 
-fun Activity.displayInSafeArea(onSafeAreaApplied : (screenResolution : ScreenResolution) -> Unit = {}) {
+fun Activity.displayInSafeArea() {
     ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
         val bars = insets.getInsets(
             WindowInsetsCompat.Type.systemBars()
@@ -128,12 +147,6 @@ fun Activity.displayInSafeArea(onSafeAreaApplied : (screenResolution : ScreenRes
         val cutout = insets.getInsets(WindowInsetsCompat.Type.displayCutout())
         if (cutout.top > 0 || cutout.left > 0 || cutout.right > 0) {
             v.setBackgroundColor(Color.BLACK)
-        }
-
-        v.post {
-            val realWidth = v.width - v.paddingLeft - v.paddingRight
-            val realHeight = v.height - v.paddingTop - v.paddingBottom
-            onSafeAreaApplied(ScreenResolution(realWidth, realHeight))
         }
 
         WindowInsetsCompat.CONSUMED
