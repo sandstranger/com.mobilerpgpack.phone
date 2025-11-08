660ba90588614691c2b2b3f45c2774a851bce125
sandstranger <sandstranger@gmail.com>
2025-11-08 17:15:47 +0400

    remove extra classes for more clear code

--- app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/DoomRPGSeriesEngineInfo.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/DoomRPGSeriesEngineInfo.kt
@@ -3,19 +3,24 @@ package com.mobilerpgpack.phone.engine.engineinfo
 import android.app.Activity
 import android.system.Os
 import androidx.compose.runtime.Composable
+import com.mobilerpgpack.phone.engine.EngineTypes
 import com.mobilerpgpack.phone.translator.ITranslationManager
 import com.mobilerpgpack.phone.ui.screen.screencontrols.DrawDoomRpgSeriesKeyboard
 import com.mobilerpgpack.phone.ui.screen.screencontrols.IScreenControlsView
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import org.koin.core.component.inject
 import kotlin.getValue
 
-abstract class DoomRPGSeriesEngineInfo(private val mainEngineLib: String,
-                                   private val allLibs : Array<String>,
-                                   private val buttonsToDraw : Collection<IScreenControlsView>) :
-    SDL2EngineInfo(mainEngineLib, allLibs, buttonsToDraw) {
+open class DoomRPGSeriesEngineInfo(
+    private val mainEngineLib: String,
+    private val allLibs: Array<String>,
+    private val buttonsToDraw: Collection<IScreenControlsView>,
+    private val activeEngineType: EngineTypes,
+    private val pathToResourceFlow: Flow<String>) :
+    SDL2EngineInfo(mainEngineLib, allLibs, buttonsToDraw, activeEngineType, pathToResourceFlow) {
 
-    private val translationManager : ITranslationManager by inject ()
+    private val translationManager: ITranslationManager by inject()
 
     override suspend fun initialize(activity: Activity) {
         super.initialize(activity)
@@ -23,16 +28,18 @@ abstract class DoomRPGSeriesEngineInfo(private val mainEngineLib: String,
         val useSdlTTFForTextRendering = preferencesStorage.useSDLTTFForFontsRendering.first()
         val enableMachineTranslation = preferencesStorage.enableGameMachineTextTranslation.first()
 
-        Os.setenv("ENABLE_SDL_TTF", useSdlTTFForTextRendering.toString().lowercase(),true)
-        Os.setenv("ENABLE_TEXTS_MACHINE_TRANSLATION",
-            enableMachineTranslation.toString().lowercase(),true)
+        Os.setenv("ENABLE_SDL_TTF", useSdlTTFForTextRendering.toString().lowercase(), true)
+        Os.setenv(
+            "ENABLE_TEXTS_MACHINE_TRANSLATION",
+            enableMachineTranslation.toString().lowercase(), true
+        )
 
         translationManager.inGame = true
         translationManager.activeEngine = engineType
     }
 
     @Composable
-    override fun DrawVirtualKeyboard(){
+    override fun DrawVirtualKeyboard() {
         super.DrawVirtualKeyboard()
         DrawDoomRpgSeriesKeyboard()
     }
