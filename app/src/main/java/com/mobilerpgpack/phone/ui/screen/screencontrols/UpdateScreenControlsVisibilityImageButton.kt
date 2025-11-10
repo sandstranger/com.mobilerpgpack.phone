91a42e108aa446064f173e4ae79419245331c13c
sandstranger <sandstranger@gmail.com>
2025-11-11 01:01:25 +0400

    fix possible bug with always showing quick panel buttons

--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/UpdateScreenControlsVisibilityImageButton.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/UpdateScreenControlsVisibilityImageButton.kt
@@ -15,7 +15,7 @@ class UpdateScreenControlsVisibilityImageButton(
     override fun onClick() {
         screenController?.activeViewsToDraw?.forEach {
             if (it !== this) {
-                if (it.isQuickPanel && it.canBeDrawn) {
+                if (it.isQuickPanel) {
                     it.canBeDrawn = false
                 } else {
                     it.canBeDrawn = !it.canBeDrawn
