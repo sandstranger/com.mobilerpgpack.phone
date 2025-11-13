1fa5c3c8953283dd4ea88673872131234a343f4b
sandstranger <sandstranger@gmail.com>
2025-11-13 22:09:35 +0400

    disable multitouch support for SDL, because it is have a lot of issues with compose and this old games

--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/SDL3ScreenController.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/SDL3ScreenController.kt
@@ -1,6 +1,5 @@
 package com.mobilerpgpack.phone.ui.screen.screencontrols
 
-import android.view.MotionEvent
 import org.libsdl3.app.SDLActivity
 import org.libsdl3.app.SDLSurface
 import org.libsdl3.app.SDLSurface.getNormalizedX
@@ -12,53 +11,13 @@ class SDL3ScreenController : SDLScreenController() {
 
     override val viewHeight: Int get() = SDLSurface.fixedHeight
 
-    override fun handlePointerAtIndex(i: Int, pointerId: Int, viewWidth : Float,
-                                      viewHeight : Float, event: MotionEvent) {
+    override fun handlePointer(pointerId: Int, pressure: Float, x: Float, y: Float, motionEvent : Int,
+                               viewWidth : Float, viewHeight : Float) {
 
-        if (!super.evenCanBeUsed(i, event)) {
-            return
-        }
-
-        val eventAction = event.actionMasked
-
-        when (event.getToolType(i)) {
-            MotionEvent.TOOL_TYPE_MOUSE -> {
-                val motionListener = SDLActivity.getMotionListener()
-                val x = motionListener.getEventX(event, i)
-                val y = motionListener.getEventY(event, i)
-                val relative = motionListener.inRelativeMode()
-                SDLActivity.onNativeMouse(event.buttonState, eventAction, x, y, relative)
-            }
-
-            MotionEvent.TOOL_TYPE_STYLUS, MotionEvent.TOOL_TYPE_ERASER -> {
-                val p = event.getPressure(i).coerceAtMost(1.0f)
-                val toolType = event.getToolType(i)
-                val buttonState =
-                    (event.buttonState shr 4) or (1 shl (if (toolType == MotionEvent.TOOL_TYPE_STYLUS) 0 else 30))
-                SDLActivity.onNativePen(
-                    pointerId,
-                    SDLActivity.getMotionListener().getPenDeviceType(event.device),
-                    buttonState,
-                    eventAction,
-                    event.getX(i),
-                    event.getY(i),
-                    p)
-            }
-
-            else -> {
-                val x = getNormalizedX(event.getX(i))
-                val y = getNormalizedY(event.getY(i))
-                val p = event.getPressure(i).coerceAtMost(1.0f)
-
-                SDLActivity.onNativeTouch(event.deviceId, pointerId,
-                    eventAction, x, y, p)
-            }
-        }
-    }
-
-    override fun onMotionEventFinished(event: MotionEvent) {
-        super.onMotionEventFinished(event)
-        SDLSurface.getScaleGestureDetector().onTouchEvent(event)
+        val normalizedX = getNormalizedX(x)
+        val normalizedY = getNormalizedY(y)
+        SDLActivity.onNativeTouch(DEFAULT_TOUCH_DEVICE_ID, pointerId,
+                    motionEvent, normalizedX, normalizedY, pressure)
     }
 
     companion object {
