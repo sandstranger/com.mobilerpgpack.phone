d31b1eca6c0c488bcd580bbb927cd57d5034f0ab
sandstranger <sandstranger@gmail.com>
2025-11-22 19:25:19 +0400

    compile zmusic from sources

---
+++ app/src/main/java/com/mobilerpgpack/phone/utils/Ini.kt
@@ -0,0 +1,68 @@
+package com.mobilerpgpack.phone.utils
+
+import org.apache.commons.configuration2.INIConfiguration
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+
+class Ini (pathToFile : String ){
+
+    private val iniFile = File(pathToFile)
+
+    private val iniConfig = INIConfiguration()
+
+    private var loaded = false
+
+    fun getBooleanValue (key: String) = getIntValue(key) > 0
+
+    fun getFloatValue (key: String) : Float {
+        if (!loaded){
+            load()
+        }
+        return if (iniConfig.containsKey(key)) iniConfig.getFloat(key) else 0.0f
+    }
+
+    fun getStringValue (key: String) : String {
+        if (!loaded){
+            load()
+        }
+        return if (iniConfig.containsKey(key)) iniConfig.getString(key) else ""
+    }
+
+    fun getIntValue (key: String) : Int{
+        if (!loaded){
+            load()
+        }
+        return if (iniConfig.containsKey(key)) iniConfig.getInt(key) else 0
+    }
+
+    fun setValue (key: String, value: Boolean) = setValue(key, if (value) 1 else 0)
+
+    fun <T> setValue (key: String, value: T){
+        if (!loaded){
+            load()
+        }
+
+        if (loaded) {
+            FileWriter(iniFile).use {
+                iniConfig.setProperty(key, value)
+                iniConfig.write(it)
+            }
+        }
+    }
+
+    fun clear(){
+        loaded = false
+        iniConfig.clear()
+    }
+
+    fun load (){
+        clear()
+        if (iniFile.exists()) {
+            FileReader(iniFile).use {
+                iniConfig.read(it)
+            }
+            loaded = true
+        }
+    }
+}
