da4f6a6ff7334e89f7dd602239b002b34f0a34e3
sandstranger <sandstranger@gmail.com>
2025-11-24 00:13:34 +0400

    use compose for showing file info

---
+++ app/src/main/java/com/mobilerpgpack/phone/ui/items/ErrorDialog.kt
@@ -0,0 +1,21 @@
+package com.mobilerpgpack.phone.ui.items
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.mobilerpgpack.phone.R
+
+@Composable
+fun ShowErrorDialog (message : String,showDialog : Boolean = false, dismissAction : (() -> Unit)? = null){
+    if (showDialog) {
+        AlertDialog(
+            title = { Text(stringResource(R.string.error)) },
+            text = { Text(message) },
+            confirmButton = {
+                TextButton(onClick = { dismissAction?.invoke() })
+                { Text(stringResource(R.string.ok_text)) }
+            }, onDismissRequest = { dismissAction?.invoke() })
+    }
+}
