825da79501534de2ca966c8398d6f491ebb444b1
sandstranger <sandstranger@gmail.com>
2025-12-04 01:12:18 +0400

    add separate mods model class to use it later for doom64

---
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/utils/ModsModel.kt
@@ -0,0 +1,60 @@
+package com.mobilerpgpack.phone.engine.engineinfo.utils
+
+import com.mobilerpgpack.phone.main.KoinModulesProvider
+import com.mobilerpgpack.phone.utils.ComposeImmutableList
+import com.mobilerpgpack.phone.utils.MutableValue
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import kotlinx.serialization.json.Json
+import org.koin.core.qualifier.named
+import org.koin.java.KoinJavaComponent.get
+import java.io.File
+
+@Serializable
+abstract class ModsModel {
+
+    protected abstract val jsonFileName : String
+
+    private val modsCollection = ComposeImmutableList<Mod>()
+
+    @Transient
+    private val jsoFile = File(pathToRootUserFolder + File.separator + jsonFileName)
+
+    val enableModsSupport = MutableValue<Boolean>()
+
+    var modsCount : Int
+        get() = modsCollection.count!!
+        set(value) {
+            modsCollection.count = value
+            save()
+        }
+
+    val pathToMods get() = modsCollection.sourceList
+
+    val pathToModsComposeCollection get() = modsCollection.composeList
+
+    init {
+        modsCollection.initialize(defaultValue = { Mod() })
+        enableModsSupport.initialize(false){
+            save()
+        }
+    }
+
+    fun updateComposeModsList () = modsCollection.updateComposeList()
+
+    fun save () = jsoFile.writeText(Json.encodeToString(this))
+
+    companion object{
+        protected val pathToRootUserFolder : String = get(String()::class.java,
+            named(KoinModulesProvider.USER_ROOT_FOLDER_NAMED_KEY))
+
+        @JvmStatic
+        protected inline fun <reified T> load(jsonFileName : String ): T where T : ModsModel {
+            val jsoFile = File(pathToRootUserFolder + File.separator + jsonFileName)
+
+            return if (jsoFile.exists())
+                Json.decodeFromString<T>(jsoFile.readText()) else
+                T::class.java.getDeclaredConstructor().newInstance()
+        }
+    }
+}
