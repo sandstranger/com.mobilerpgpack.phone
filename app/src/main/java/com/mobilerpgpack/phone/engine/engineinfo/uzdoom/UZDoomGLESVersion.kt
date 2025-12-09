222d52c4103b7914aebf2db07fe177bd0f21988a
sandstranger <sandstranger@gmail.com>
2025-12-09 20:43:34 +0400

    allow to changes uzdoom gles version

---
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/uzdoom/UZDoomGLESVersion.kt
@@ -0,0 +1,15 @@
+package com.mobilerpgpack.phone.engine.engineinfo.uzdoom
+
+enum class UZDoomGLESVersion (val value : Int){
+    OpenGLES_2_0 (2),
+    OpenGLES_3_0 (3),
+    OpenGLES_3_2 (4);
+
+    companion object {
+        val stringCollection = entries.map { it.toString() }.toList()
+
+        fun fromValue(value: Int): UZDoomGLESVersion {
+            return entries.find { it.value == value }!!
+        }
+    }
+}
