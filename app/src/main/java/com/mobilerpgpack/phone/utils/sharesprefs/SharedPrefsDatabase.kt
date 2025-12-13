9e0935b62df0eaa79a49206ee9876d7c5bebf5ca
sandstranger <sandstranger@gmail.com>
2025-12-13 15:02:27 +0400

    base shared prefs api working with sqlite based on data store api

---
+++ app/src/main/java/com/mobilerpgpack/phone/utils/sharesprefs/SharedPrefsDatabase.kt
@@ -0,0 +1,25 @@
+package com.mobilerpgpack.phone.utils.sharesprefs
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import org.koin.java.KoinJavaComponent.get
+
+@Database(entities = [SharedPrefsEntry::class], version = 1)
+internal abstract class SharedPrefsDatabase : RoomDatabase() {
+
+    abstract fun dao(): SharedPrefsDatabase
+
+    companion object {
+        const val DATABASE_NAME = "shared_prefs"
+        private const val FULL_DATABASE_NAME = "$DATABASE_NAME.db"
+
+        fun createInstance(): SharedPrefsDatabase {
+            val context : Context = get (Context::class.java)
+            return Room.databaseBuilder(context.applicationContext,
+                SharedPrefsDatabase::class.java,
+                FULL_DATABASE_NAME).build()
+        }
+    }
+}
