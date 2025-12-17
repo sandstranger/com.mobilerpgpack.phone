5671e2e57cc4abbdde1c429f2e7e9f759ae15c38
sandstranger <sandstranger@gmail.com>
2025-12-17 21:49:10 +0400

    hint color improvements in compose edittext

--- app/src/main/java/com/mobilerpgpack/phone/ui/items/EditTextItem.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/items/EditTextItem.kt
@@ -1,8 +1,8 @@
 package com.mobilerpgpack.phone.ui.items
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
@@ -14,7 +14,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -30,7 +29,6 @@ import com.mobilerpgpack.phone.ui.getOnSurfaceVariantColor
 import com.mobilerpgpack.phone.ui.getPrimaryColor
 import com.mobilerpgpack.phone.ui.getSurfaceContainerHighColor
 import com.mobilerpgpack.phone.ui.getTextButtonsColors
-import com.mobilerpgpack.phone.ui.getTextFieldColors
 
 @Composable
 fun EditTextItem(
@@ -49,20 +47,25 @@ fun EditTextItem(
     val surfaceContainerHighColor = getSurfaceContainerHighColor()
     val textButtonsColor = getTextButtonsColors()
     val onBackgroundColor = getOnBackgroundColor()
+    val hintColor = Color.Gray
+    val textColor = if (tempValue.isEmpty() && hint.isNotEmpty()) hintColor else onBackgroundColor
+    val showText = tempValue.isNotEmpty() || hint.isNotEmpty()
 
     Column(modifier = Modifier
         .fillMaxWidth()
         .clickable { showDialog = true }
-        .padding(16.dp)) {
+        .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp)) {
 
         Text(text = title, color = onBackgroundColor)
 
-        Text(
-            text = tempValue.ifEmpty { hint },
-            style = MaterialTheme.typography.bodyMedium,
-            color = if (value.isNotEmpty()) Color.Unspecified else Color.Gray,
-            modifier = Modifier.padding(top = 4.dp)
-        )
+        if (showText) {
+            Text(
+                text = tempValue.ifEmpty { hint },
+                style = MaterialTheme.typography.bodyMedium,
+                color = textColor,
+            )
+        }
     }
 
     if (showDialog) {
@@ -90,7 +93,9 @@ fun EditTextItem(
                 OutlinedTextField(
                     value = tempValue,
                     onValueChange = { tempValue = it },
-                    placeholder = { Text(hint, color = onSurfaceVariantColor) },
+                    placeholder = { if (tempValue.isEmpty() && hint.isNotEmpty()) {
+                        Text(hint, color = hintColor)
+                    } },
                     singleLine = singleLine,
                     colors = getEditTextFieldColors(),
                     modifier = Modifier.fillMaxWidth(),
