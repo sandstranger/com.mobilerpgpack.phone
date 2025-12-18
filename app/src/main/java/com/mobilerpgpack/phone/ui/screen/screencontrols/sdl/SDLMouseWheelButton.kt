79caf7d24e24b2578b289000a2ff4b3a90500b6d
sandstranger <sandstranger@gmail.com>
2025-12-18 23:45:00 +0400

    allow to emulate continous mouse wheel events

--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/sdl/SDLMouseWheelButton.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/sdl/SDLMouseWheelButton.kt
@@ -3,8 +3,14 @@ package com.mobilerpgpack.phone.ui.screen.screencontrols.sdl
 import android.view.MotionEvent
 import com.mobilerpgpack.phone.engine.EngineTypes
 import com.mobilerpgpack.phone.ui.screen.screencontrols.ControlsType
+import com.mobilerpgpack.phone.ui.screen.screencontrols.MouseViewState
 import com.mobilerpgpack.phone.ui.screen.screencontrols.ViewState
 import com.mobilerpgpack.phone.ui.screen.screencontrols.ViewRenderRule
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 abstract class SDLMouseWheelButton(
     id: String,
@@ -19,21 +25,60 @@ abstract class SDLMouseWheelButton(
     defaultViewRenderRule: ViewRenderRule = ViewRenderRule.Default,
     controlsType: ControlsType = ControlsType.Default,isDeleted : Boolean = false,
     consumeTouchEventsByDefault : Boolean = true,
-    ignoreOutOfBoundsTouchEvents : Boolean = false) :
+    ignoreOutOfBoundsTouchEvents : Boolean = false,
+    invokeWheelEventsWhilePressingDefaultState : Boolean = false) :
     SDLImageButton(id, engineType, offsetXPercent, offsetYPercent, sizePercent,
         alpha, Int.MIN_VALUE, buttonResId, useToggle = false,
         defaultViewRenderRule,controlsType, isDeleted, consumeTouchEventsByDefault, ignoreOutOfBoundsTouchEvents) {
+
+    private val scope = CoroutineScope(Dispatchers.Default)
+
+    private val mouseViewState = MouseViewState(
+        id,
+        engineType,
+        offsetXPercent = offsetXPercent,
+        offsetYPercent = offsetYPercent,
+        sizePercent = sizePercent,
+        buttonResId = buttonResId,
+        alpha = alpha,
+        defaultViewRenderRule = defaultViewRenderRule,
+        controlsType = controlsType,
+        isDeletedInitialState = isDeleted,
+        invokeWheelEventsWhilePressingDefaultState = invokeWheelEventsWhilePressingDefaultState)
 
     init {
         show = !isQuickPanel
     }
 
-    final override fun onTouchDown(keyCode: Int) =
-        onMouseWheel(keyCode,0f, if (wheelUp) DEFAULT_POSITION else -DEFAULT_POSITION, MotionEvent.ACTION_SCROLL)
+    override val viewState get() = mouseViewState
 
-    final override fun onTouchUp(keyCode: Int) {}
+    final override fun onTouchDown(keyCode: Int) {
+        if (mouseViewState.invokeWheelEventsWhilePressing){
+            scope.launch { mouseWheelAsync(keyCode) }
+        }
+        else{
+            mouseWheel(keyCode)
+        }
+    }
+
+    final override fun onTouchUp(keyCode: Int) {
+        scope.coroutineContext.cancelChildren()
+    }
 
     protected abstract fun onMouseWheel (keyCode: Int, x : Float, y : Float, event: Int )
+
+    private suspend fun mouseWheelAsync(keyCode: Int){
+        val wheelPosition = if (wheelUp) 100.0f else -1f * 100f
+        while (true){
+            onMouseWheel(keyCode,0f, wheelPosition, MotionEvent.ACTION_SCROLL)
+            delay(100)
+        }
+    }
+
+    private fun mouseWheel(keyCode: Int){
+        onMouseWheel(keyCode,0f, if (wheelUp) DEFAULT_POSITION else -DEFAULT_POSITION,
+            MotionEvent.ACTION_SCROLL)
+    }
 
     private companion object{
         private const val DEFAULT_POSITION = 500.0f
