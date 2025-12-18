c71d44aa991bdc9f784f2924df763f64bffaba02
sandstranger <sandstranger@gmail.com>
2025-12-18 10:05:11 +0400

    change color to normal in edit mode

--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/sdl/SDLImageButton.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/sdl/SDLImageButton.kt
@@ -82,7 +82,8 @@ abstract class SDLImageButton(
         Image(painter = painterResource(id = viewState.buttonResId),
             contentDescription = id,
             modifier = Modifier.interactiveControlModifier(isEditMode, inGame).let{
-                if (isPressed) it.graphicsLayer { colorFilter = ColorFilter.tint(color = Color.Yellow) } else it
+                if (isPressed && !isEditMode && inGame)
+                    it.graphicsLayer { colorFilter = ColorFilter.tint(color = Color.Yellow) } else it
             } )
     }
 
