c903253e0b54a387b1fb959cadb8dde41e510a32
sandstranger <sandstranger@gmail.com>
2025-12-18 23:45:00 +0400

    allow to emulate continous mouse wheel events

---
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/MouseViewState.kt
@@ -0,0 +1,45 @@
+package com.mobilerpgpack.phone.ui.screen.screencontrols
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.mobilerpgpack.phone.engine.EngineTypes
+import kotlinx.coroutines.flow.first
+
+class MouseViewState(id: String,
+                     engineType: EngineTypes,
+                     offsetXPercent: Float = 0f,
+                     offsetYPercent: Float = 0f,
+                     sizePercent: Float = 0.13f,
+                     alpha: Float = 0.65f,
+                     sdlKeyEvent : Int = Int.MIN_VALUE,
+                     buttonResId: Int = NOT_EXISTING_RES,
+                     defaultViewRenderRule: ViewRenderRule = ViewRenderRule.Default,
+                     isDeletedInitialState : Boolean = false,
+                     controlsType: ControlsType = ControlsType.Default,
+                     private val invokeWheelEventsWhilePressingDefaultState : Boolean = false) :
+    ViewState (id, engineType, offsetXPercent, offsetYPercent, sizePercent,
+    alpha, sdlKeyEvent, buttonResId,defaultViewRenderRule,isDeletedInitialState,controlsType,
+        false,false,true,true,
+        false,false) {
+
+    private val invokeWheelEventsWhilePressingPrefsKey = "invoke_wheel_events_while_pressing"
+
+    var invokeWheelEventsWhilePressing by mutableStateOf(invokeWheelEventsWhilePressingDefaultState)
+
+    override suspend fun load() {
+        super.load()
+        invokeWheelEventsWhilePressing = preferencesStorage.getBooleanValue(invokeWheelEventsWhilePressingPrefsKey,
+            invokeWheelEventsWhilePressingDefaultState).first()
+    }
+
+    override fun save() {
+        super.save()
+        preferencesStorage.setBooleanValue(invokeWheelEventsWhilePressingPrefsKey, invokeWheelEventsWhilePressing)
+    }
+
+    override fun resetToDefaults() {
+        super.resetToDefaults()
+        invokeWheelEventsWhilePressing = invokeWheelEventsWhilePressingDefaultState
+    }
+}
