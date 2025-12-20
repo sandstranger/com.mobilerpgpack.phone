00ab772218bc73bfb91ddaf504b2fd5d9af815ef
sandstranger <sandstranger@gmail.com>
2025-12-20 11:34:43 +0400

    completely delete dark theme code overriding

--- app/src/main/java/com/mobilerpgpack/phone/ui/items/CustomNavigationBar.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/items/CustomNavigationBar.kt
@@ -1,15 +1,22 @@
 package com.mobilerpgpack.phone.ui.items
 
 import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.toArgb
 import androidx.core.view.WindowCompat
 import com.mobilerpgpack.phone.ui.getSurfaceColor
+import com.mobilerpgpack.phone.utils.PreferencesStorage
+import org.koin.compose.koinInject
 
 @Suppress("DEPRECATION")
 @Composable
-fun SetupNavigationBar(useDarkTheme: Boolean) {
+fun SetupNavigationBar() {
+    val preferencesStorage : PreferencesStorage = koinInject()
+    val useDarkTheme by preferencesStorage.getUseDarkThemeValue().collectAsState(initial = false)
     val activity = LocalActivity.current!!
     val colorCodeToUse = getSurfaceColor().toArgb()
 
