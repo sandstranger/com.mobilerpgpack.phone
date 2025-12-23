3d75b9edd318f78f7d8e6247cfb4fa8df082f6ee
sandstranger <sandstranger@gmail.com>
2025-12-23 15:20:27 +0400

    move others items to composable

--- app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/doomrpgseries/DoomRpgComposeSettings.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/doomrpgseries/DoomRpgComposeSettings.kt
@@ -9,16 +9,16 @@ import com.mobilerpgpack.phone.engine.engineinfo.IEngineInfo
 import com.mobilerpgpack.phone.ui.items.prefsitems.DrawHorizontalDivider
 import com.mobilerpgpack.phone.ui.items.prefsitems.RequestPath
 import com.mobilerpgpack.phone.ui.items.prefsitems.RequestPathMode
-import kotlinx.coroutines.launch
-import org.koin.core.component.inject
+import com.mobilerpgpack.phone.utils.PreferencesStorage
+import org.koin.compose.koinInject
 import org.koin.core.qualifier.named
 
 class DoomRpgComposeSettings : CommonDoomRpgComposeSettings() {
 
-    private val engineInfo : IEngineInfo by inject (named(EngineTypes.DoomRpg.toString()))
-
     @Composable
     override fun DrawSettings(navController: NavHostController) {
+        val engineInfo : IEngineInfo = koinInject (named(EngineTypes.DoomRpg.name))
+        val preferencesStorage : PreferencesStorage = koinInject()
         val savedPathToDoomRpgZip = preferencesStorage.pathToDoomRpgZipFile
         RequestPath(
             stringResource(R.string.doom_rpg_zip_file),
