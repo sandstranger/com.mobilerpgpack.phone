406c6e2d555c3d9dfee642346735f21f7ed9a153
sandstranger <sandstranger@gmail.com>
2025-12-23 12:22:56 +0400

    correctly reload ini values

---
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/perfectdark/PerfectDarkComposeSettings.kt
@@ -0,0 +1,11 @@
+package com.mobilerpgpack.phone.engine.engineinfo.perfectdark
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.mobilerpgpack.phone.engine.engineinfo.IEngineUIController
+
+class PerfectDarkComposeSettings : IEngineUIController {
+    @Composable
+    override fun DrawSettings(navController: NavHostController) {
+    }
+}
