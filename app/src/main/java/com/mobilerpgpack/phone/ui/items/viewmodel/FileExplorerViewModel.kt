158044868cb9eeeb29078b8b5e7e0ae7015f8a98
sandstranger <sandstranger@gmail.com>
2025-12-23 14:07:41 +0400

    dismiss fileexprorer after screen orientation was changed to avoid crashes

---
+++ app/src/main/java/com/mobilerpgpack/phone/ui/items/viewmodel/FileExplorerViewModel.kt
@@ -0,0 +1,8 @@
+package com.mobilerpgpack.phone.ui.items.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.codekidlabs.storagechooser.StorageChooser
+
+class FileExplorerViewModel : ViewModel() {
+    var storageChooser : StorageChooser? = null
+}
