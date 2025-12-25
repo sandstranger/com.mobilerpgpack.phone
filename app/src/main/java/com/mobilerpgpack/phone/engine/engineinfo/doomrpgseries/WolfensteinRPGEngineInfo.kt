891a0fadbd3ac41367272e8f69f8a484da495ea1
sandstranger <sandstranger@gmail.com>
2025-12-25 21:24:42 +0400

    fix bug with not found resources for doomrpg series

---
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/doomrpgseries/WolfensteinRPGEngineInfo.kt
@@ -0,0 +1,9 @@
+package com.mobilerpgpack.phone.engine.engineinfo.doomrpgseries
+
+import com.mobilerpgpack.phone.engine.EngineTypes
+
+class WolfensteinRPGEngineInfo(mainEngineLib: String, allLibs: Array<String>) :
+    DoomRPGSeriesEngineInfo(mainEngineLib, allLibs, EngineTypes.WolfensteinRpg) {
+
+    override val pathToResource: String get() = preferencesStorage.pathToWolfensteinRpgIpaFile
+}
