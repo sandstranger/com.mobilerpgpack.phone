dfcfd72eef05cc5768806ff1bafad5357130a91d
sandstranger <sandstranger@gmail.com>
2025-12-25 22:44:08 +0400

    onstick controls perfect dark layout

--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/layout/PerfectDarkControlsLayout.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/layout/PerfectDarkControlsLayout.kt
@@ -3,6 +3,7 @@ package com.mobilerpgpack.phone.ui.screen.screencontrols.layout
 import android.view.KeyEvent
 import com.mobilerpgpack.phone.R
 import com.mobilerpgpack.phone.engine.EngineTypes
+import com.mobilerpgpack.phone.ui.screen.screencontrols.ControlsType
 import com.mobilerpgpack.phone.ui.screen.screencontrols.IScreenControlsView
 import com.mobilerpgpack.phone.ui.screen.screencontrols.UpdateQuickPanelVisibilityImageButton
 import com.mobilerpgpack.phone.ui.screen.screencontrols.UpdateScreenControlsVisibilityImageButton
@@ -55,18 +56,18 @@ val perfectDarkAbsoluteTouchControlsLayout: Collection<IScreenControlsView> = li
         ignoreOutOfBoundsTouchEvents = true
     ),
     SDL2MouseWheelButton(
-        "prev_weapon",
+        "next_weapon",
         EngineTypes.PerfectDark,
-        wheelUp = true,
+        wheelUp = false,
         offsetXPercent = 0.85f,
         offsetYPercent = 0.18f,
         sizePercent = 0.06f,
         buttonResId = R.drawable.next_weapon,
     ),
     SDL2MouseWheelButton(
-        "next_weapon",
+        "prev_weapon",
         EngineTypes.PerfectDark,
-        wheelUp = false,
+        wheelUp = true,
         offsetXPercent = 0.85f,
         offsetYPercent = 0.32f,
         sizePercent = 0.06f,
@@ -160,4 +161,168 @@ val perfectDarkAbsoluteTouchControlsLayout: Collection<IScreenControlsView> = li
         offsetYPercent = 0.03f,
         sizePercent = 0.05f,
         buttonResId = R.drawable.toggles
+    ))
+
+val perfectDarkOnScreenStickControlsLayout: Collection<IScreenControlsView> = listOf(
+    SDL2Dpad(
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.03f,
+        offsetYPercent = 0.16f,
+        sizePercent = 0.16f,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2OnScreenStick(
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.8f,
+        offsetYPercent = 0.54f,
+        sizePercent = 0.21f,
+        stickType = StickType.RightStick,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2OnScreenStick(
+        engineType = EngineTypes.PerfectDark,
+        offsetXPercent = 0.05f,
+        offsetYPercent = 0.54f,
+        sizePercent = 0.21f,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    UpdateQuickPanelVisibilityImageButton(
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.1f,
+        offsetYPercent = 0.03f,
+        sizePercent = 0.05f,
+        buttonResId = R.drawable.quick_panel,
+        controlsType = ControlsType.OnScreenStick,
+        isDeleted = true
+    ),
+    SDL2ImageButton(
+        "attack",
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.72f,
+        offsetYPercent = 0.44f,
+        sizePercent = 0.11f,
+        buttonResId = R.drawable.attack_button,
+        sdlKeyEvent = KeyEvent.KEYCODE_SPACE,
+        consumeTouchEventsByDefault = false,
+        ignoreOutOfBoundsTouchEvents = true,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2MouseWheelButton(
+        "next_weapon",
+        EngineTypes.PerfectDark,
+        wheelUp = false,
+        offsetXPercent = 0.85f,
+        offsetYPercent = 0.18f,
+        sizePercent = 0.06f,
+        buttonResId = R.drawable.next_weapon,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2MouseWheelButton(
+        "prev_weapon",
+        EngineTypes.PerfectDark,
+        wheelUp = true,
+        offsetXPercent = 0.85f,
+        offsetYPercent = 0.32f,
+        sizePercent = 0.06f,
+        buttonResId = R.drawable.prev_weapon,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2ImageButton(
+        "escape",
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.99f,
+        offsetYPercent = 0.05f,
+        sizePercent = 0.065f,
+        buttonResId = R.drawable.pause,
+        sdlKeyEvent = KeyEvent.KEYCODE_ESCAPE,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2ImageButton(
+        "use",
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.97f,
+        offsetYPercent = 0.45f,
+        sizePercent = 0.047f,
+        buttonResId = R.drawable.use,
+        sdlKeyEvent = KeyEvent.KEYCODE_E,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2ImageButton(
+        "aim_mode",
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.72f,
+        offsetYPercent = 0.26f,
+        sizePercent = 0.055f,
+        buttonResId = R.drawable.zoom,
+        sdlKeyEvent = KeyEvent.KEYCODE_Z,
+        useToggle = true,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2ImageButton(
+        "reload",
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.73f,
+        offsetYPercent = 0.88f,
+        sizePercent = 0.055f,
+        buttonResId = R.drawable.machine_gun_magazine,
+        sdlKeyEvent = KeyEvent.KEYCODE_R,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2ImageButton(
+        "fire_mode",
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.99f,
+        offsetYPercent = 0.2f,
+        sizePercent = 0.047f,
+        buttonResId = R.drawable.reload_gun,
+        sdlKeyEvent = KeyEvent.KEYCODE_F,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2ImageButton(
+        "radial_menu",
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.99f,
+        offsetYPercent = 0.33f,
+        sizePercent = 0.047f,
+        buttonResId = R.drawable.circles_ext,
+        sdlKeyEvent = KeyEvent.KEYCODE_Q,
+        useToggle = true,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2ImageButton(
+        "full_crouch",
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.63f,
+        offsetYPercent = 0.88f,
+        sizePercent = 0.055f,
+        buttonResId = R.drawable.smash_arrows,
+        sdlKeyEvent = KeyEvent.KEYCODE_CTRL_LEFT,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    SDL2ImageButton(
+        "half_crouch",
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.53f,
+        offsetYPercent = 0.88f,
+        sizePercent = 0.055f,
+        buttonResId = R.drawable.crouch,
+        sdlKeyEvent = KeyEvent.KEYCODE_SHIFT_LEFT,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    ShowSDLKeyboardButton(
+        KeyboardType.SDL2Keyboard,
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.76f,
+        offsetYPercent = 0.005f,
+        sizePercent = 0.08f,
+        buttonResId = R.drawable.keyboard,
+        controlsType = ControlsType.OnScreenStick
+    ),
+    UpdateScreenControlsVisibilityImageButton(
+        EngineTypes.PerfectDark,
+        offsetXPercent = 0.62f,
+        offsetYPercent = 0.03f,
+        sizePercent = 0.05f,
+        buttonResId = R.drawable.toggles,
+        controlsType = ControlsType.OnScreenStick
     ))
