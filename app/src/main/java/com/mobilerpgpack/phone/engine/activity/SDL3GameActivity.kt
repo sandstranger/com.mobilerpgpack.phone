3a733b6b19cee9c123fec538e03d7a889006f612
sandstranger <sandstranger@gmail.com>
2025-12-28 19:01:17 +0400

    enable OnBackInvokedCallback

--- app/src/main/java/com/mobilerpgpack/phone/engine/activity/SDL3GameActivity.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/activity/SDL3GameActivity.kt
@@ -1,7 +1,9 @@
 package com.mobilerpgpack.phone.engine.activity
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.window.OnBackInvokedDispatcher
 import androidx.activity.enableEdgeToEdge
 import com.mobilerpgpack.phone.engine.engineinfo.IEngineInfo
 import com.mobilerpgpack.phone.engine.engineinfo.isResourceCorrect
@@ -40,6 +42,13 @@ internal class SDL3GameActivity : SDLActivity(), KoinComponent {
             super.onCreate(savedInstanceState)
             loadLayout()
             onNativeLibrariesLoaded()
+            if (Build.VERSION.SDK_INT >= 33) {
+                onBackInvokedDispatcher.registerOnBackInvokedCallback(
+                    OnBackInvokedDispatcher.PRIORITY_DEFAULT
+                ) {
+                    onBackPressed()
+                }
+            }
         }
     }
 
