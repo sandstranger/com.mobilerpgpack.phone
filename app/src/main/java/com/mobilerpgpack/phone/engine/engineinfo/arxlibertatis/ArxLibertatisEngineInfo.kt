6001f11787a3b64ad26aeb0f33c86702d3b8ad19
sandstranger <sandstranger@gmail.com>
2025-12-29 02:23:53 +0400

    base DI modules for arx fatalis

--- app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/arxlibertatis/ArxLibertatisEngineInfo.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/arxlibertatis/ArxLibertatisEngineInfo.kt
@@ -1,7 +1,6 @@
 package com.mobilerpgpack.phone.engine.engineinfo.arxlibertatis
 
 import com.mobilerpgpack.phone.engine.EngineTypes
-import com.mobilerpgpack.phone.engine.engineinfo.EngineInfo
 import com.mobilerpgpack.phone.engine.engineinfo.sdl.SDL2EngineInfo
 
 class ArxLibertatisEngineInfo (mainEngineLib: String, allLibs: Array<String>): SDL2EngineInfo(mainEngineLib, allLibs,
