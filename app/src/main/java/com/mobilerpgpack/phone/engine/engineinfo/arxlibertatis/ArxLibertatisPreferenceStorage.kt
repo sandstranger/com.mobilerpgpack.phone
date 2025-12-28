9836aef5c0b83776da2bd187f05cb3bbf2532b8c
sandstranger <sandstranger@gmail.com>
2025-12-29 01:27:34 +0400

    update cfg init from dev branch

---
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/arxlibertatis/ArxLibertatisPreferenceStorage.kt
@@ -0,0 +1,11 @@
+package com.mobilerpgpack.phone.engine.engineinfo.arxlibertatis
+
+import com.mobilerpgpack.phone.utils.PreferencesStorage
+import com.mobilerpgpack.phone.utils.sharesprefs.stringPreferencesKey
+
+class ArxLibertatisPreferenceStorage : PreferencesStorage() {
+
+    val pathToArxFatalisFolderPrefsKey = stringPreferencesKey("path_to_arx_fatalis_folder")
+
+
+}
