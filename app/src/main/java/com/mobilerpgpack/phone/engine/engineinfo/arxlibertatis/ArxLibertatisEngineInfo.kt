aff0caf4ca0bb7c29bf8c745e9dc764a90e437b3
sandstranger <sandstranger@gmail.com>
2026-01-01 19:09:53 +0400

    correctly changing safe area size in arx fatalis

--- app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/arxlibertatis/ArxLibertatisEngineInfo.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/arxlibertatis/ArxLibertatisEngineInfo.kt
@@ -2,6 +2,7 @@ package com.mobilerpgpack.phone.engine.engineinfo.arxlibertatis
 
 import com.mobilerpgpack.phone.engine.EngineTypes
 import com.mobilerpgpack.phone.engine.engineinfo.sdl.SDL2EngineInfo
+import com.mobilerpgpack.phone.utils.ScreenResolution
 import com.sun.jna.Native
 import org.koin.core.component.inject
 import org.koin.core.qualifier.named
@@ -9,6 +10,8 @@ import java.io.File
 
 class ArxLibertatisEngineInfo(mainEngineLib: String, allLibs: Array<String>) :
     SDL2EngineInfo(mainEngineLib, allLibs, EngineTypes.ArxLibertatis) {
+
+    private var customResolutionWasSet = false
 
     private val arxPreferenceStorage by inject<ArxLibertatisPreferenceStorage>(
         named(EngineTypes.ArxLibertatis.name))
@@ -53,10 +56,24 @@ class ArxLibertatisEngineInfo(mainEngineLib: String, allLibs: Array<String>) :
 
     private external fun updateScreenControlsHidingState (controlsHided : Boolean)
 
+    private external fun onSafeAreaApplied(screenWidth: Int, screenHeight : Int)
+
     override fun onNativeLibrariesLoaded() {
         super.onNativeLibrariesLoaded()
         Native.register(ArxLibertatisEngineInfo::class.java, mainLibraryName)
         updateScreenControlsHidingState(preferencesStorage.hideScreenControls)
+    }
+
+    override fun setScreenResolution(screenResolution: ScreenResolution) {
+        super.setScreenResolution(screenResolution)
+        customResolutionWasSet = true
+    }
+
+    override fun onSafeAreaApplied(screenResolution: ScreenResolution) {
+        super.onSafeAreaApplied(screenResolution)
+        if (!customResolutionWasSet) {
+            onSafeAreaApplied(screenResolution.screenWidth, screenResolution.screenHeight)
+        }
     }
 
     private companion object {
