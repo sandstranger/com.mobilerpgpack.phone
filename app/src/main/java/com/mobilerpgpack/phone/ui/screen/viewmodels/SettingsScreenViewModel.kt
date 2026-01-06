e969a6f6d9927794a77634646a005ffb29133035
sandstranger <sandstranger@gmail.com>
2026-01-07 02:36:01 +0400

    move source folder to class

--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/viewmodels/SettingsScreenViewModel.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/viewmodels/SettingsScreenViewModel.kt
@@ -2,13 +2,8 @@ package com.mobilerpgpack.phone.ui.screen.viewmodels
 
 import android.app.Activity
 import android.content.Context
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.mobilerpgpack.phone.engine.EngineTypes
-import com.mobilerpgpack.phone.main.KoinModulesProvider
 import com.mobilerpgpack.phone.utils.IAssetExtractor
 import com.mobilerpgpack.phone.utils.PreferencesStorage
 import com.mobilerpgpack.phone.utils.copyFolder
@@ -16,9 +11,7 @@ import com.mobilerpgpack.phone.utils.startGame
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.get
 import org.koin.core.component.inject
-import org.koin.core.qualifier.named
 import java.io.File
 
 internal class SettingsScreenViewModel : ViewModel(), KoinComponent {
@@ -31,6 +24,8 @@ internal class SettingsScreenViewModel : ViewModel(), KoinComponent {
 
     private val assetsExtractor : IAssetExtractor by inject ()
 
+    private val sourceFolder = context.getExternalFilesDir("")!!.absolutePath
+    
     @Volatile
     private var contentCopied = true
 
@@ -48,7 +43,6 @@ internal class SettingsScreenViewModel : ViewModel(), KoinComponent {
         startGame(activity, activeEngine)
 
     fun copyContentFromInternalStorage () {
-        val sourceFolder = context.getExternalFilesDir("")!!.absolutePath
         val targetFolder = preferencesStorage.pathToRootUserFolder
         if (!contentCopied || sourceFolder == targetFolder) {
             return
