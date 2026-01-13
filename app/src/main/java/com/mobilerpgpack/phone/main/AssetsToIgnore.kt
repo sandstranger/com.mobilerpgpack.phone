121403d679988c8420257fd93c8ef5078dd7e2bd
sandstranger <sandstranger@gmail.com>
2026-01-13 15:43:49 +0400

    always load default fteqw config on Android

--- app/src/main/java/com/mobilerpgpack/phone/main/AssetsToIgnore.kt
+++ app/src/main/java/com/mobilerpgpack/phone/main/AssetsToIgnore.kt
@@ -1,5 +1,3 @@
 package com.mobilerpgpack.phone.main
 
-import com.mobilerpgpack.phone.engine.engineinfo.fteqw.FTEQWEngineInfo
-
-val assetsToIgnoreChecking = listOf("com.codelobster", "uzdoom.ini", "cfg.ini", FTEQWEngineInfo.FTEQW_CONFIGS_DIR)
+val assetsToIgnoreChecking = listOf("com.codelobster", "uzdoom.ini", "cfg.ini")
