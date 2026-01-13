0de68c20f8731edb80e807c595dab5a76e82b5fa
sandstranger <sandstranger@gmail.com>
2026-01-13 04:17:39 +0400

    move to consts

--- app/src/main/java/com/mobilerpgpack/phone/main/AssetsToIgnore.kt
+++ app/src/main/java/com/mobilerpgpack/phone/main/AssetsToIgnore.kt
@@ -1,3 +1,5 @@
 package com.mobilerpgpack.phone.main
 
-val assetsToIgnoreChecking = listOf("com.codelobster", "uzdoom.ini", "cfg.ini", "FTEQW")
+import com.mobilerpgpack.phone.engine.engineinfo.fteqw.FTEQWEngineInfo
+
+val assetsToIgnoreChecking = listOf("com.codelobster", "uzdoom.ini", "cfg.ini", FTEQWEngineInfo.FTEQW_CONFIGS_DIR)
