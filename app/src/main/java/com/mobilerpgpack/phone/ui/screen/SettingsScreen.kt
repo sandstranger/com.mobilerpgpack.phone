44b0a00d6b65492778ad697206f238e94283a76b
sandstranger <sandstranger@gmail.com>
2026-01-15 23:16:13 +0400

    restore missing code

--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/SettingsScreen.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/SettingsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -69,6 +70,10 @@ class SettingsScreen : ComposeScreen(SCREEN_NAME) {
         val settingsScreenViewModel : SettingsScreenViewModel = koinViewModel ()
         val useFloatingStartGameButton = preferencesStorage.useFloatingStartGameButton.getComposableValue()
 
+        LaunchedEffect(Unit) {
+            drawFloatingActionButton.value = preferencesStorage.useFloatingStartGameButton.value
+        }
+        
         super.onFloatingActionButtonClickedDelegate = {
             settingsScreenViewModel.onStartGameClicked(activeEngine, activity)
         }
