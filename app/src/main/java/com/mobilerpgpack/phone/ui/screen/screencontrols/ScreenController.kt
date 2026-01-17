9224d52a32c8dbf532d4a063211e83d3e3ab9aea
sandstranger <sandstranger@gmail.com>
2026-01-17 21:13:10 +0400

    fix compiling

--- app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/ScreenController.kt
+++ app/src/main/java/com/mobilerpgpack/phone/ui/screen/screencontrols/ScreenController.kt
@@ -42,7 +42,6 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
