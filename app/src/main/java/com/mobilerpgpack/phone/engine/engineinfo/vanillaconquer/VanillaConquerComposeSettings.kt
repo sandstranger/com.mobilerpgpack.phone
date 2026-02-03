b16bea90a5cccc8fd805937f84ec9b6c3c609725
sandstranger <sandstranger@gmail.com>
2026-02-03 19:54:00 +0400

    DI module for C&C

---
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/vanillaconquer/VanillaConquerComposeSettings.kt
@@ -0,0 +1,12 @@
+package com.mobilerpgpack.phone.engine.engineinfo.vanillaconquer
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.mobilerpgpack.phone.engine.engineinfo.IEngineUIController
+
+class VanillaConquerComposeSettings : IEngineUIController {
+    @Composable
+    override fun DrawSettings(navController: NavHostController) {
+
+    }
+}
