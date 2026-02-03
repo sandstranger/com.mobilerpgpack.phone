995f0b8c5fc072c438336d04f0b22a8caed08add
sandstranger <sandstranger@gmail.com>
2026-02-03 20:26:33 +0400

    base C&C ui code

--- app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/vanillaconquer/VanillaConquerComposeSettings.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/vanillaconquer/VanillaConquerComposeSettings.kt
@@ -1,12 +1,42 @@
 package com.mobilerpgpack.phone.engine.engineinfo.vanillaconquer
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import com.mobilerpgpack.phone.R
+import com.mobilerpgpack.phone.engine.EngineTypes
 import com.mobilerpgpack.phone.engine.engineinfo.IEngineUIController
+import com.mobilerpgpack.phone.ui.items.prefsitems.DrawHorizontalDivider
+import com.mobilerpgpack.phone.ui.items.prefsitems.ListPreferenceItem
+import com.mobilerpgpack.phone.ui.items.prefsitems.RequestPath
+import com.mobilerpgpack.phone.utils.getComposableValue
+import org.koin.compose.koinInject
+import org.koin.core.qualifier.named
 
 class VanillaConquerComposeSettings : IEngineUIController {
     @Composable
     override fun DrawSettings(navController: NavHostController) {
-
+        val prefsStorage : VanillaConquerPreferencesStorage = koinInject(named(EngineTypes.VanillaConquer.name))
+        prefsStorage.run {
+            val activeVanillaConquerGame = prefsStorage.activeVanillaConquerGame.getComposableValue(
+                VanillaConquerGames.DefaultGame)
+            ListPreferenceItem(stringResource(R.string.vanilla_conquer_active_game),
+                activeVanillaConquerGame){
+                setEnumValue(activeVanillaConquerGamePrefsKey,it)
+            }
+            DrawHorizontalDivider()
+            when (activeVanillaConquerGame) {
+                VanillaConquerGames.TiberianDawn -> {
+                    RequestPath(stringResource(R.string.path_to_tiberian_dawn_resources),
+                        pathToTiberianDawnResources,
+                        pathToTiberianDawnResourcesPrefsKey)
+                }
+                VanillaConquerGames.RedAlert -> {
+                    RequestPath(stringResource(R.string.path_to_red_alert_resources),
+                        pathToRedAlertResources,
+                        pathToRedAlertResourcesPrefsKey)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/vanillaconquer/VanillaConquerEngineInfo.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/vanillaconquer/VanillaConquerEngineInfo.kt
@@ -2,10 +2,59 @@ package com.mobilerpgpack.phone.engine.engineinfo.vanillaconquer
 
 import com.mobilerpgpack.phone.engine.EngineTypes
 import com.mobilerpgpack.phone.engine.engineinfo.sdl.SDL2EngineInfo
+import com.mobilerpgpack.phone.main.RED_ALERT_NATIVE_LIB_NAME
+import com.mobilerpgpack.phone.main.TIBERIAN_DAWN_NATIVE_LIB_NAME
+import com.mobilerpgpack.phone.utils.PreferencesStorage
+import com.sun.jna.Native
+import org.koin.core.component.inject
+import org.koin.core.qualifier.named
 
 class VanillaConquerEngineInfo :
     SDL2EngineInfo ("", emptyArray(), EngineTypes.VanillaConquer) {
+    private val prefsStorage : VanillaConquerPreferencesStorage by inject (
+        named(EngineTypes.VanillaConquer.name))
+
+    private val activeGame get() = prefsStorage.activeVanillaConquerGame.value!!
+
+    private val redAlertNativeLibs : Array<String> by inject (
+        named(VanillaConquerGames.RedAlert.name))
+
+    private val tiberianDawnNativeLibs : Array<String> by inject (
+        named(VanillaConquerGames.TiberianDawn.name))
+
+    override val mouseButtonsEventsCanBeInvoked = true
+
+    override val loadGL4ES = false
+
+    override val enableGyroscope = false
+
+    override val touchFullScreenModeCanBeUsed = false
 
     override val pathToResource: String
-        get() = TODO("Not yet implemented")
+        get() = when (activeGame) {
+            VanillaConquerGames.TiberianDawn -> prefsStorage.pathToTiberianDawnResources.value!!
+            VanillaConquerGames.RedAlert -> prefsStorage.pathToRedAlertResources.value!!
+        }
+
+    override val nativeLibraries: Array<String>
+        get() = when (activeGame) {
+            VanillaConquerGames.TiberianDawn -> tiberianDawnNativeLibs
+            VanillaConquerGames.RedAlert -> redAlertNativeLibs
+        }
+
+    override val mainLibraryName: String
+        get() = when (activeGame) {
+            VanillaConquerGames.TiberianDawn -> TIBERIAN_DAWN_NATIVE_LIB_NAME
+            VanillaConquerGames.RedAlert -> RED_ALERT_NATIVE_LIB_NAME
+        }
+
+    override val preferencesStorage: PreferencesStorage get() = prefsStorage
+
+    private external fun setPathToResources (pathToResource : String)
+
+    override fun onNativeLibrariesLoaded() {
+        super.onNativeLibrariesLoaded()
+        Native.register(VanillaConquerEngineInfo::class.java, mainLibraryName)
+        setPathToResources(pathToResource)
+    }
 }
