749d8053ee8b0133c2b3f4803b116b6f2e2f4359
sandstranger <sandstranger@gmail.com>
2026-02-05 17:45:05 +0400

    added initial C&C ui android values

--- app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/vanillaconquer/VanillaConquerComposeSettings.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/vanillaconquer/VanillaConquerComposeSettings.kt
@@ -6,9 +6,12 @@ import androidx.navigation.NavHostController
 import com.mobilerpgpack.phone.R
 import com.mobilerpgpack.phone.engine.EngineTypes
 import com.mobilerpgpack.phone.engine.engineinfo.IEngineUIController
+import com.mobilerpgpack.phone.ui.items.EditTextItem
+import com.mobilerpgpack.phone.ui.items.prefsitems.DrawCommandLinePreferences
 import com.mobilerpgpack.phone.ui.items.prefsitems.DrawHorizontalDivider
 import com.mobilerpgpack.phone.ui.items.prefsitems.ListPreferenceItem
 import com.mobilerpgpack.phone.ui.items.prefsitems.RequestPath
+import com.mobilerpgpack.phone.ui.items.prefsitems.SwitchPreferenceItem
 import com.mobilerpgpack.phone.utils.getComposableValue
 import org.koin.compose.koinInject
 import org.koin.core.qualifier.named
@@ -20,6 +23,12 @@ class VanillaConquerComposeSettings : IEngineUIController {
         prefsStorage.run {
             val activeVanillaConquerGame = prefsStorage.activeVanillaConquerGame.getComposableValue(
                 VanillaConquerGames.DefaultGame)
+
+            DrawCommandLinePreferences(vanillaConquerCommandLineArgs,
+                vanillaConquerCommandLineArgsPrefsKey.name)
+
+            DrawHorizontalDivider()
+
             ListPreferenceItem(stringResource(R.string.vanilla_conquer_active_game),
                 activeVanillaConquerGame){
                 setEnumValue(activeVanillaConquerGamePrefsKey,it)
@@ -36,6 +45,33 @@ class VanillaConquerComposeSettings : IEngineUIController {
                         pathToRedAlertResources,
                         pathToRedAlertResourcesPrefsKey)
                 }
+            }
+            DrawHorizontalDivider()
+
+            SwitchPreferenceItem(stringResource(R.string.psydoom_enable_vsync),
+                vanillaConquerEnableVsync,vanillaConquerEnableVsyncPrefsKey.name)
+
+            DrawHorizontalDivider()
+
+            SwitchPreferenceItem(stringResource(R.string.enable_dos_mode),
+                enableDosMode,enableDosModePrefsKey.name)
+
+            DrawHorizontalDivider()
+
+            EditTextItem(stringResource(R.string.framerate_limit), vanillaConquerFrameRateLimit){
+                setIntValue(vanillaConquerFrameRateLimitPrefsKey, it.coerceAtLeast(10))
+            }
+
+            DrawHorizontalDivider()
+
+            EditTextItem(stringResource(R.string.mouse_sensitivity), vanillaConquerMouseSensitivity){
+                setIntValue(vanillaConquerMouseSensitivityPrefsKey, it.coerceAtLeast(10))
+            }
+
+            DrawHorizontalDivider()
+
+            EditTextItem(stringResource(R.string.controller_pointer_speed), vanillaConquerControllerPointerSpeed){
+                setIntValue(vanillaConquerControllerPointerSpeedPrefsKey, it.coerceAtLeast(1))
             }
         }
     }
