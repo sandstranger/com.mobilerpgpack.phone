3776967f97a810cb3eb29d98c13ceef9873663ef
sandstranger <sandstranger@gmail.com>
2026-02-16 23:21:53 +0400

    base openxray kotlin classes

---
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/openxray/OpenXrayGames.kt
@@ -0,0 +1,10 @@
+package com.mobilerpgpack.phone.engine.engineinfo.openxray
+
+enum class OpenXrayGames {
+    ClearSky,
+    CallOfPripyat;
+
+    companion object{
+        val DefaultGame = CallOfPripyat
+    }
+}
