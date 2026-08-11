a6109423a8501e5e6c6bef719fba7e0223049020
sandstranger <sandstranger@gmail.com>
2026-08-11 17:19:04 +0400

    fix: fixed wrong screen resolution resizing in doom 64 on Android platform

--- app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/doom64/Doom64EngineInfo.kt
+++ app/src/main/java/com/mobilerpgpack/phone/engine/engineinfo/doom64/Doom64EngineInfo.kt
@@ -1,13 +1,10 @@
 package com.mobilerpgpack.phone.engine.engineinfo.doom64
 
-import android.system.Os
-import androidx.activity.ComponentActivity
 import com.mobilerpgpack.phone.engine.EngineTypes
 import com.mobilerpgpack.phone.engine.engineinfo.sdl.SDL3EngineInfo
 import com.mobilerpgpack.phone.engine.engineinfo.utils.Mod
 import com.mobilerpgpack.phone.engine.engineinfo.utils.ModsModel
 import com.mobilerpgpack.phone.engine.engineinfo.utils.modsCanBeUsed
-import com.mobilerpgpack.phone.utils.ScreenResolution
 import com.sun.jna.Native
 import org.koin.core.component.inject
 import org.koin.core.qualifier.named
@@ -17,9 +14,6 @@ open class Doom64EngineInfo(
     mainEngineLib: String,
     allLibs: Array<String>) :
     SDL3EngineInfo(mainEngineLib, allLibs, EngineTypes.Doom64ExPlus) {
-
-    private var savedScreenResolution : ScreenResolution? = null
-    private var customScreenResolutionWasApplied = false
     private val modsModel : ModsModel by inject (named(EngineTypes.Doom64ExPlus.toString()))
 
     override val gl4esShaderCacheFolderName = "doom64_gl4es_cache"
@@ -57,7 +51,6 @@ open class Doom64EngineInfo(
 
     private external fun MouseCursorCanBeDrawn() : Boolean
     private external fun setScreenResolution (screenWidth : Int, screenHeight : Int)
-    private external fun RecalculateScreenResolution(screenWidth : Int, screenHeight : Int)
     private external fun setPathsToResources (pathToWadsFolder : String, pathToUserFolder : String)
     private external fun updateMaxAnisotropyValue (targetAnisotropyValue : Int)
 
@@ -66,23 +59,6 @@ open class Doom64EngineInfo(
         Native.register(Doom64EngineInfo::class.java, mainLibraryName)
         setPathsToResources(pathToResource, getPathToDoom64UserFolder())
         updateMaxAnisotropyValue(preferencesStorage.doom64AnisotropyTexturesValue.value!!)
-        savedScreenResolution?.run {
-            setScreenResolution(screenWidth, screenHeight)
-        }
-    }
-
-    final override fun setScreenResolution(screenResolution: ScreenResolution) {
-        super.setScreenResolution(screenResolution)
-        savedScreenResolution = screenResolution
-        customScreenResolutionWasApplied = true
-    }
-
-    final override fun onSafeAreaApplied(screenResolution: ScreenResolution) {
-        super.onSafeAreaApplied(screenResolution)
-        if (!customScreenResolutionWasApplied) {
-            setScreenResolution(screenResolution.screenWidth, screenResolution.screenHeight)
-            RecalculateScreenResolution(screenResolution.screenWidth, screenResolution.screenHeight)
-        }
     }
 
     final override fun isMouseShown() = MouseCursorCanBeDrawn()
